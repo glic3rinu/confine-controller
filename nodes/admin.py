d75a85899c30d99ef5a094718222e1a8221a179b
marcay <marcay@pangea.org>
2012-04-23 13:38:57 +0200

    nodes.admin

--- nodes/admin.py
+++ nodes/admin.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
-from models import Node, Storage, Memory, CPU, CommunityLink, GatewayLink, LocalLink, DirectLink
+from models import Node, Storage, Memory, CPU, Interface
 from django import forms
 import settings
+from django.utils.html import escape
+from utils.admin import admin_link_factory
 
 class StorageInlineForm(forms.ModelForm):
     types = forms.MultipleChoiceField(choices=settings.STORAGE_CHOICES)
@@ -19,32 +21,39 @@ class CPUInline(admin.TabularInline):
     model = CPU
     max_num = 0
     
-class CommunityLinkInline(admin.TabularInline):
-    model = CommunityLink
-    extra = 0
-    
-class GatewayLinkInline(admin.TabularInline):
-    model = GatewayLink
-    extra = 0
-    
-class LocalLinkInline(admin.TabularInline):
-    model = LocalLink
-    extra = 0
-    
-class DirectLinkInline(admin.TabularInline):
-    model = DirectLink
-    extra = 0
+class InterfaceInline(admin.TabularInline):
+    model = Interface
+    extra = 1
     
 
-def url_link(self):
-    return '<a href="%s">%s' % (self.url, self.url)
-url_link.short_description = "URL"
-url_link.allow_tags = True
+NODE_STATE_COLORS = { settings.ONLINE: "green",
+                      settings.PROJECTED: "darkorange",
+                      settings.DISABLED: "gray",
+                      settings.OFFLINE: "red",}
+
+def colored_state(node):
+    state = escape(node.state)
+    color = NODE_STATE_COLORS.get(node.state, "black")
+    return """<b><span style="color: %s;">%s</span></b>""" % (color, state)
+colored_state.short_description = "State" 
+colored_state.allow_tags = True
+colored_state.admin_order_field = 'state'
+
 
 class NodeAdmin(admin.ModelAdmin):
-    list_display = ['hostname', url_link, 'architecture', 'status' ]
-    list_filter = ['architecture', 'status']
-    inlines = [CPUInline, MemoryInline, StorageInline, CommunityLinkInline, GatewayLinkInline, LocalLinkInline, DirectLinkInline]
+    list_display = ['hostname', admin_link_factory('url', description='URL'), 'architecture', colored_state ]
+    list_filter = ['architecture', 'state']
+    inlines = [CPUInline, MemoryInline, StorageInline, InterfaceInline]
+    fieldsets = (
+        (None, {
+            'fields': (('hostname',), ('state',), ('architecture',), ('public_key',), ('uci',))
+        }),
+        ('Community node', {
+            'fields': (('url',), ('latitude',), ('longitude',))
+        }),
+    )
+
 
 
 admin.site.register(Node, NodeAdmin)
+

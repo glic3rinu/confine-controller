83aa3c72da41886dc701e02261458a6617578c81
Marc Aymerich <glicerinu@gmail.com>
2012-05-29 20:44:47 +0200

    minor fixes

--- scripts/vct_server_extensions.sh
+++ scripts/vct_server_extensions.sh
@@ -4,24 +4,20 @@ set -u
 
 . ./confine.functions
 
-# SERVER_IMAGE_URL='http://images.confine-project.eu/server/debian-amd64-container-image-v0-latest.ext4.tgz'
-SERVER_IMAGE_URL='http://images.confine-project.eu/server/debian-amd64-container-image.ext4.tgz'
+SERVER_IMAGE_URL='http://images.confine-project.eu/server/debian-amd64-container-image-v0-latest.ext4.tgz'
 
 vct_server_customize () {
+ 
     # Prepare Image
     #TODO: image directory?
     wget -O /tmp/server.image.tgz $SERVER_IMAGE_URL 
-    
-    if [ $0 ]; then 
+    if [ $? -eq 0 ]; then 
         local IMAGE_NAME=$(tar xvfz /tmp/server.image.tgz --directory /tmp/)
     else
         server_deployment.sh --type bootable --image /tmp/server.img
         local IMAGE_NAME="server.img"
     fi
-    
     local MOUNT_POINT=$(mktmp -p /tmp)
-
-    
     trap "rm -fr $MOUNT_POINT" INT TERM EXIT
     mount /tmp/$IMAGE_NAME $MOUNT_POINT || { echo 'Error mounting image' >&2; exit 1; }
     trap "umount $MOUNT_POINT && rm -fr $MOUNT_POINT || { echo 'Error umounting' >&2; exit 1; }" INT TERM EXIT
@@ -33,7 +29,7 @@ vct_server_customize () {
     chmod 700 $MOUNT_POINT/home/confine/.ssh/
     mkdir -p $MOUNT_POINT/root/.ssh
     chmod 700 $MOUNT_POINT/root/.ssh
-    # chroot $MOUNT_POINT ssh-keygen -P '' -f /root/.ssh/confine
+    # chroot $MOUNT_POINT ssh-keygen -P '' -f /root/.ssh/id_rsa
     cp $VCT_KEYS_DIR/server_ssh.pub $MOUNT_POINT/home/confine/.ssh/
     cp $VCT_KEYS_DIR/server_ssh $MOUNT_POINT/home/confine/.ssh/
 
@@ -69,4 +65,12 @@ vct_server_customize () {
     umount $MOUNT_POINT && rm -fr $MOUNT_POINT || { echo 'Error umounting' >&2; exit 1; } 
     trap - INT TERM EXIT
 }
+
+
+
+# mkdir /dev/net
+# mknod /dev/net/tun c 10 200
+# chmod 0666 /dev/net/tun
+
+# ip tuntap add mode tun tun0
 

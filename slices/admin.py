81c706359e1ac8c5988ecb7159f26c865ce25aab
Marc Aymerich <confine@controller.confine-project.eu>
2012-09-27 18:35:15 +0200

    Customize slices admin iface

--- slices/admin.py
+++ slices/admin.py
@@ -27,6 +27,9 @@ class PrivateIfaceInline(admin.TabularInline):
 
 
 class SliverAdmin(admin.ModelAdmin):
+    list_display = ['description', 'id', 'instance_sn', 'node', 'slice']
+    list_filter = ['slice__name',]
+    search_fields = ['description', 'node__description', 'slice__name']
     inlines = [SliverPropInline, IsolatedIfaceInline, PublicIfaceInline, PrivateIfaceInline]
 
 
@@ -37,7 +40,6 @@ class SliverInlineForm(forms.ModelForm):
     """
     #FIXME: js needed: when save popup the main form is not updated with the new/changed slivers
     #TODO: possible reimplementation when nested inlines support becomes available 
-    #      on django.contrib.admin
     sliver = forms.CharField(label="Sliver", widget=ShowText(bold=True))
     node = forms.CharField(label="Node", widget=ShowText(bold=True))
     url = forms.CharField(label="Node CN URL", widget=ShowText(bold=True))
@@ -51,7 +53,8 @@ class SliverInlineForm(forms.ModelForm):
         if 'instance' in kwargs:
             instance = kwargs['instance']
             sliver_change = reverse('admin:slices_sliver_change', args=(instance.pk,))
-            self.initial['sliver'] = mark_safe("<a href='%s' id='add_id_user' onclick='return showAddAnotherPopup(this);'>%s </a>" % (sliver_change, instance))
+            self.initial['sliver'] = mark_safe("""<a href='%s' id='add_id_user' 
+                onclick='return showAddAnotherPopup(this);'>%s </a>""" % (sliver_change, instance))
             node_change = reverse('admin:nodes_node_change', args=(instance.node.pk,))
             self.initial['node'] = mark_safe("<a href='%s'>%s</a>" % (node_change, instance.node))
             self.initial['url'] = mark_safe("<a href='%s'>%s</a>" % (instance.node.cn_url, 
@@ -70,12 +73,19 @@ class SlicePropInline(admin.TabularInline):
 
 
 class SliceAdmin(admin.ModelAdmin):
-    list_display = ['name', 'uuid', 'instance_sn', 'vlan_nr', 'set_state', 'template', 'expires_on']
+    list_display = ['name', 'uuid', 'instance_sn', 'vlan_nr', 'set_state',
+        'template', 'expires_on']
     list_filter = ['set_state']
     date_hierarchy = 'expires_on'
+    search_fields = ['name', 'uuid']
     inlines = [SlicePropInline,SliverInline]
 
 
+class TemplateAdmin(admin.ModelAdmin):
+    list_display = ['name', 'description', 'type', 'arch', 'data', 'is_active']
+    list_filter = ['is_active', 'type', 'arch']
+    search_fields = ['name', 'description', 'type', 'arch']
+
 admin.site.register(Sliver, SliverAdmin)
 admin.site.register(Slice, SliceAdmin)
-admin.site.register(Template)
+admin.site.register(Template, TemplateAdmin)

50caa1cb5997186dc027dcc69ec6574a864dd922
glic3rinu <marcay@pangea.org>
2012-09-28 15:02:26 +0200

    Customized issues admin iface

--- issues/admin.py
+++ issues/admin.py
@@ -17,10 +17,35 @@ class TicketInline(admin.TabularInline):
 class TicketAdmin(admin.ModelAdmin):
     list_display = ['id', 'subject', admin_link_factory('created_by'), 
         admin_link_factory('owner'), admin_link_factory('queue', app_model='issues_queue'),
-        'priority', 'state']
+        'priority', 'state', 'created_on', 'last_modified_on']
     #TODO: create a list filter for 'owner__username'
-    list_filter = ['queue', 'priority', 'state']
+    list_filter = ['queue__name', 'priority', 'state']
+    date_hierarchy = 'created_on'
+    search_fields = ['id', 'subject', 'created_by__username', 'created_by__email', 
+        'queue', 'owner__username']
     inlines = [MessageInline]
+    readonly_fields = ('created_by',)
+    fieldsets = (
+        (None, {
+            'fields': ('created_by', 'subject', ('queue', 'owner'), ('priority', 'state'))
+        }),
+        ('CC', {
+            'classes': ('collapse',),
+            'fields': ('cc',)
+        }),)
+    add_fieldsets = (
+        (None, {
+            'fields': ('subject', ('queue', 'owner'), ('priority', 'state'))
+        }),
+        ('CC', {
+            'classes': ('collapse',),
+            'fields': ('cc',)
+        }),)
+
+    def get_fieldsets(self, request, obj=None):
+        if not obj:
+            return self.add_fieldsets
+        return super(UserAdmin, self).get_fieldsets(request, obj)
 
 
 class QueueAdmin(admin.ModelAdmin):

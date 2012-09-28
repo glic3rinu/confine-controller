40ca5c7854b2ab505b911e5842b27131a3ff9e44
glic3rinu <marcay@pangea.org>
2012-09-28 13:35:58 +0200

    Customized tinc admin iface

--- nodes/forms.py
+++ nodes/forms.py
@@ -5,6 +5,8 @@ from django.utils.safestring import mark_safe
 
 class HostInlineAdminForm(forms.ModelForm):
     host = forms.CharField(label="Host", widget=ShowText(bold=True))
+    pk = forms.CharField(label="ID", widget=ShowText(bold=True))
+    tinc_name = forms.CharField(label="Tinc Name", widget=ShowText())
 
     class Meta:
         fields = []
@@ -16,6 +18,8 @@ class HostInlineAdminForm(forms.ModelForm):
             host_change = reverse('admin:nodes_host_change', args=(instance.pk,))
             self.initial['host'] = mark_safe("""<a href='%s' id='add_id_user' 
                 onclick='return showAddAnotherPopup(this);'>%s </a>""" % (host_change, instance))
+            self.initial['pk'] = instance.pk
+            self.initial['tinc_name'] = instance.tinc_name
 
 
 class NodeInlineAdminForm(forms.ModelForm):
@@ -26,7 +30,7 @@ class NodeInlineAdminForm(forms.ModelForm):
     node = forms.CharField(label="Node", widget=ShowText(bold=True))
     pk = forms.CharField(label="ID", widget=ShowText(bold=True))
     cn_url = forms.CharField(label="Node CN URL", widget=ShowText(bold=True))
-    rd_arch = forms.CharField(label="RD Arch", widget=ShowText(bold=True))
+    rd_arch = forms.CharField(label="RD Arch", widget=ShowText())
     set_state = forms.CharField(label="Set State", widget=ShowText(bold=True))
 
     class Meta:
--- tinc/admin.py
+++ tinc/admin.py
@@ -1,6 +1,21 @@
 from django.contrib import admin
-from models import TincAddress, Island
+from models import TincAddress, Island, Gateway
 
 
-admin.site.register(TincAddress)
-admin.site.register(Island)
+class TincAddressAdmin(admin.ModelAdmin):
+    list_display = ['ip_addr', 'port', 'island', 'server']
+    list_filter = ['island__name', 'port']
+    search_fields = ['ip_addr', 'island__name', 'island__description', 'server__tinc_name'] 
+
+
+class IslandAdmin(admin.ModelAdmin):
+    list_display = ['name', 'id', 'description']
+    search_fields = ['name', 'description']
+
+
+class GatewayAdmin(admin.ModelAdmin):
+    list_display = ['tinc_name', 'id' ]
+
+admin.site.register(TincAddress, TincAddressAdmin)
+admin.site.register(Island, IslandAdmin)
+admin.site.register(Gateway, GatewayAdmin)

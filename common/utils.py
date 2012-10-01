cc6b70cfa8e5bd295d3941abd7657fb8d819093e
glic3rinu <marcay@pangea.org>
2012-10-01 12:32:43 +0200

    Fixed colored_field.short_description

--- common/utils.py
+++ common/utils.py
@@ -1,7 +1,7 @@
 
-def get_field(obj, field_name):
+def get_field_value(obj, field_name):
     names = field_name.split('__')
-    rel = getattr(obj, names.pop())
+    rel = getattr(obj, names.pop(0))
     for name in names:
         rel = getattr(rel, name)
     return rel

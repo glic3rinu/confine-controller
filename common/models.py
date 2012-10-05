77934816a179e2e9c0db851995a699575d7fe7fb
glic3rinu <marcay@pangea.org>
2012-10-05 15:06:47 +0200

    Added admin actions for issues.tickets

--- common/models.py
+++ common/models.py
@@ -1,1 +1,20 @@
 from django.db import models
+
+def generate_chainer_manager(qs_class):
+    # Allow chained managers
+    # Based on http://djangosnippets.org/snippets/562/#c2486
+    class ChainerManager(models.Manager):
+        def __init__(self):
+            super(ChainerManager,self).__init__()
+            self.queryset_class = qs_class
+
+        def get_query_set(self):
+            return self.queryset_class(self.model)
+
+        def __getattr__(self, attr, *args):
+            try:
+                return getattr(self.__class__, attr, *args)
+            except AttributeError:
+                return getattr(self.get_query_set(), attr, *args)
+    return ChainerManager()
+

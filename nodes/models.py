14b28fc2ef10f34041d2ea39439d823a839f2cff
Marc Aymerich <confine@controller.confine-project.eu>
2012-10-05 23:50:29 +0200

    Fixed bits here and there

--- nodes/models.py
+++ nodes/models.py
@@ -16,15 +16,15 @@ class Node(models.Model):
     description = models.CharField(max_length=256)
     admin = models.ForeignKey(User, help_text="""The user who administrates this 
         node (its creator by default).""")
-    priv_ipv4_prefix = models.GenericIPAddressField(protocol='IPv4', null=True, blank=True,
-        help_text="""IPv4 /24 network in CIDR notation used as a node private IPv4 prefix. 
-        See <a href="http://wiki.confine-project.eu/arch:addressing">addressing</a> 
-        for legal values. When null, use the default value provided in 
+    priv_ipv4_prefix = models.GenericIPAddressField(protocol='IPv4', null=True, 
+        blank=True, help_text="""IPv4 /24 network in CIDR notation used as a node 
+        private IPv4 prefix. See <a href="http://wiki.confine-project.eu/arch:addressing">
+        addressing</a> for legal values. When null, use the default value provided in 
         <a href="http://wiki.confine-project.eu/arch:rest-api#base_at_server">
         testbed parameters</a>.""")
-    sliver_mac_prefix = models.PositiveSmallIntegerField(max_length=16, null=True, blank=True,
-        help_text="""A 16-bit integer number in 0x-prefixed hexadecimal notation
-        used as the node sliver MAC prefix. See 
+    sliver_mac_prefix = models.PositiveSmallIntegerField(max_length=16, null=True,
+        blank=True, help_text="""A 16-bit integer number in 0x-prefixed hexadecimal 
+        notation used as the node sliver MAC prefix. See 
         <a href="http://wiki.confine-project.eu/arch:addressing">addressing</a> 
         for legal values. When null, use the default value provided in 
         <a href="http://wiki.confine-project.eu/arch:rest-api#base_at_server">
@@ -67,14 +67,20 @@ class CnHost(models.Model):
 
 
 class ResearchDevice(CnHost):
-    uuid = fields.UUIDField(auto=True, primary_key=True)
+    uuid = fields.UUIDField(auto=True, primary_key=True, help_text="""A universally 
+        unique identifier (UUID, RFC 4122).""")
     node = models.OneToOneField(Node)
-    pubkey = models.TextField(unique=True, blank=True, verbose_name="Public Key")
-    cert = models.TextField(unique=True, blank=True, verbose_name="Certificate")
-    arch = models.CharField(verbose_name="Architecture", max_length=16, 
-        choices=settings.RESEARCH_DEVICE_ARCHS, default=settings.DEFAULT_RESEARCH_DEVICE_ARCH)
+    pubkey = models.TextField(unique=True, blank=True, help_text="""A PEM-encoded 
+        RSA public key for this RD (used by SFA).""")
+    cert = models.TextField(unique=True, blank=True, help_text="""An X.509 
+        PEM-encoded certificate for this RD. The certificate may be signed by a 
+        CA recognised in the testbed and required by clients and services accessing 
+        the node API.""")
+    arch = models.CharField(max_length=16, choices=settings.RESEARCH_DEVICE_ARCHS, 
+        default=settings.DEFAULT_RESEARCH_DEVICE_ARCH, help_text="""Architecture 
+        of this RD (as reported by uname -m).""")
     boot_sn = models.IntegerField(default=0, help_text="""Number of times this 
-        node's RD has been instructed to be rebooted.""")
+        RD has been instructed to be rebooted.""")
     local_iface = models.CharField(verbose_name="Local Interface", max_length=16, 
         default='eth0', help_text="""Name of the interface used as a local interface. 
         See <a href="wiki.confine-project.eu/arch:node">node architecture</a>.""")
@@ -95,8 +101,6 @@ class RdDirectIface(models.Model):
 
 
 class Server(SingletonModel, CnHost):
-    #TODO: ID = 2
-
     class Meta:
         verbose_name = "Server"
         verbose_name_plural = "Server"

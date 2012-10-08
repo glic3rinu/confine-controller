888393f4626d82f25ced505b8561dcf452b4f1e8
glic3rinu <marcay@pangea.org>
2012-10-08 11:55:17 +0200

    Updated tinc and slices data model and dynamicallize tinc.name

--- slices/models.py
+++ slices/models.py
@@ -19,32 +19,37 @@ class Template(models.Model):
 
 
 class Slice(models.Model):
+    REGISTER = 'register'
     INSTANTIATE = 'instantiate'
     ACTIVATE = 'activate'
-    STATES = ((INSTANTIATE, 'Instantiate'),
+    STATES = ((REGISTER, 'Register'),
+              (INSTANTIATE, 'Instantiate'),
               (ACTIVATE, 'Activate'),)
 
     name = models.CharField(max_length=64)
     uuid = fields.UUIDField(auto=True, unique=True)
-    pubkey = models.TextField(help_text="""A PEM-encoded RSA public key for this 
-        slice (used by SFA).""")
+    pubkey = models.TextField(null=True, blank=True, help_text="""A PEM-encoded 
+        RSA public key for this slice (used by SFA).""")
     description = models.TextField(blank=True)
     expires_on = models.DateField(null=True, blank=True, help_text="""Expiration 
         date of this slice. Automatically deleted once expires.""")
-    instance_sn = models.IntegerField(help_text="""The number of times this slice 
-        has been instructed to be reset (instance sequence number).""")
-    vlan_nr = models.IntegerField(help_text="""A VLAN number allocated to this 
-        slice by the server. The only values that can be /set/ are null (no VLAN 
-        wanted) and -1 (asks the server to allocate a new VLAN number (2 <= 
-        vlan_nr < 0xFFF) on slice instantiation).""")
-    exp_data = models.FileField(verbose_name="Experiment Data",
+    instance_sn = models.PositiveIntegerField(default=0, help_text="""The number
+        of times this slice has been instructed to be reset (instance sequence 
+        number).""")
+    vlan_nr = models.IntegerField(null=True, blank=True, help_text="""A VLAN 
+        number allocated to this slice by the server. The only values that can 
+        be /set/ are null (no VLAN wanted) and -1 (asks the server to allocate a 
+        new VLAN number (2 <= vlan_nr < 0xFFF) on slice instantiation).""")
+    exp_data = models.FileField(help_text="Experiment Data",
         upload_to=settings.SLICE_EXP_DATA_DIR)
-    set_state = models.CharField(max_length=16, choices=STATES, default=INSTANTIATE)
+    set_state = models.CharField(max_length=16, choices=STATES, default=REGISTER)
     template = models.ForeignKey(Template)
-    users = models.ManyToManyField(User)
+    users = models.ManyToManyField(User, help_text="""A ist of users able to login
+        as root in slivers using their authentication tokens (usually an SSH key).""")
 
     def __unicode__(self):
         return self.name
+
 
 class SliceProp(models.Model):
     slice = models.ForeignKey(Slice)
@@ -57,8 +62,9 @@ class SliceProp(models.Model):
 
 class Sliver(models.Model):
     description = models.CharField(max_length=256)
-    instance_sn = models.IntegerField(help_text="""The number of times this sliver 
-        has been instructed to be reset (instance sequence number).""")
+    instance_sn = models.PositiveIntegerField(default=0, help_text="""The number 
+        of times this sliver has been instructed to be reset (instance sequence 
+        number).""")
     slice = models.ForeignKey(Slice)
     node = models.ForeignKey('nodes.Node')
     
@@ -106,4 +112,5 @@ class PublicIface(IpSliverIface):
 
 class PrivateIface(IpSliverIface):
     sliver = models.OneToOneField(Sliver)
+
 

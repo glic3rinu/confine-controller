59a5a37b14d367c56e7bce22e90b1217538fff38
Marc Aymerich <confine@controller.confine-project.eu>
2012-10-08 16:54:40 +0200

    Updated admin menu API URL

--- controller/menu.py
+++ controller/menu.py
@@ -39,4 +39,4 @@ class CustomMenu(Menu):
             ))
 
         self.children += [
-                    items.MenuItem('API', 'https://api.controller.confine-project.eu'),]
+                    items.MenuItem('API', 'https://controller.confine-project.eu/confine'),]

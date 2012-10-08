ac4b76feef9ec3de01fdb54e0c94085d5bfc181d
glic3rinu <marcay@pangea.org>
2012-10-08 16:21:01 +0200

    Added rest_framework to INSTALLED_APPS and started to implement nodes and base REST resources

---
+++ controller/views.py
@@ -0,0 +1,25 @@
+from rest_framework.views import APIView
+from rest_framework.response import Response
+
+BASE_API_URL = 'http://controller.confine-project.eu:8888/confine'
+
+class Base(APIView):
+    def get(self, request, format=None):
+        testbed_params = {
+            "mgmt_ipv6_prefix": "2001:db8:cafe::/48",
+            "priv_ipv4_prefix_dflt": "192.168.157.0/24",
+            "sliver_mac_prefix_dflt": "0x06ab", }
+
+        output = {"testbed_params": testbed_params,
+                  "server_href": "%s/server" % BASE_API_URL,
+                  "nodes_href": "%s/nodes/" % BASE_API_URL,
+                  "slices_href": "%s/slices/" % BASE_API_URL,
+                  "slivers_href": "%s/slivers/" % BASE_API_URL,
+                  "users_href": "%s/users/" % BASE_API_URL,
+                  "gateways_href": "%s/gateways/" % BASE_API_URL,
+                  "hosts_href": "%s/hosts/" % BASE_API_URL,
+                  "templates_href": "%s/templates/" % BASE_API_URL,
+                  "islands_href": "%s/islands/"% BASE_API_URL,}
+
+        return Response(output)
+
--- nodes/views.py
+++ nodes/views.py
@@ -1,1 +1,15 @@
-# Create your views here.
+from nodes.models import Node
+from nodes.serializers import NodeSerializer
+from rest_framework import generics
+
+
+class NodeRoot(generics.ListCreateAPIView):
+    model = Node
+    serializer_class = NodeSerializer
+
+
+class NodeInstance(generics.RetrieveUpdateDestroyAPIView):
+    model = Node
+    serializer_class = NodeSerializer
+
+

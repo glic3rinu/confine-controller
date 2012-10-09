e5caf141b18efb318579f66815ff337204646fc1
glic3rinu <marcay@pangea.org>
2012-10-09 17:50:27 +0200

    Cleaned nodes api

--- nodes/urls.py
+++ nodes/urls.py
@@ -1,23 +1,9 @@
 from django.conf.urls import patterns, url
 from nodes.views import Nodes, Node
-from nodes.resources import NodeResource
-
-node_root = NodeResource.as_view(actions={
-    'get': 'list',
-    'post': 'create'
-})
-
-node_instance = NodeResource.as_view(actions={
-    'get': 'retrieve',
-    'put': 'update',
-    'delete': 'destroy'
-})
 
 
 urlpatterns = patterns('nodes.views',
     url(r'^$', Nodes.as_view(), name='node-list'),
     url(r'^(?P<pk>[0-9]+)$', Node.as_view(), name="node-detail"),
-    url(r'^rata$', node_root),
-    url(r'^rata/(?P<pk>[0-9]+)$', node_instance),
     )
 

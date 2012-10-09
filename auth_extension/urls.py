a55d9b5da2745a4646990abcafbe92320dfa473a
Marc Aymerich <confine@controller.confine-project.eu>
2012-10-09 11:37:42 +0200

    Regenerated migrations

---
+++ auth_extension/urls.py
@@ -0,0 +1,9 @@
+from auth_extension.views import UserRoot, UserInstance
+from django.conf.urls import patterns, url
+
+
+urlpatterns = patterns('nodes.views',
+    url(r'^$', UserRoot.as_view()),
+    url(r'^(?P<pk>[0-9]+)$', UserInstance.as_view(), name="user_instance")
+)
+

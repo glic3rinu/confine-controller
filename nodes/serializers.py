764d49971909b9be62575ce51d2a0d6edf1aad85
glic3rinu <marcay@pangea.org>
2012-10-10 15:42:24 +0200

    Cosmetics

--- nodes/serializers.py
+++ nodes/serializers.py
@@ -2,7 +2,7 @@ from rest_framework import serializers
 from nodes.models import Node, ResearchDevice
 from tinc.serializers import TincHostSerializer, TincClientSerializer
 
-#TODO dynamically hook tinc serializers instead of hardcoding them
+# TODO dynamically hook tinc serializers instead of hardcoding them
 
 class ResearchDeviceSerializer(serializers.ModelSerializer):
     tinc = TincClientSerializer()

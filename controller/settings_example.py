4e3ae44ec4b2141b8191f94fec7fe0de68b7fc1c
Marc Aymerich <confine@controller.confine-project.eu>
2012-10-12 22:19:59 +0200

    Customized REST API web Page

--- controller/settings_example.py
+++ controller/settings_example.py
@@ -119,6 +119,7 @@ ROOT_URLCONF = 'controller.urls'
 WSGI_APPLICATION = 'controller.wsgi.application'
 
 TEMPLATE_DIRS = (
+    '/home/confine/controller/controller/templates',
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.

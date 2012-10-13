b43f6275056b27135e1825afa4293fe6e67acdf8
Marc Aymerich <confine@controller.confine-project.eu>
2012-10-13 18:21:06 +0200

    More cosmetics

--- auth_extension/serializers.py
+++ auth_extension/serializers.py
@@ -6,10 +6,13 @@ from rest_framework import serializers
 class UserProfileSerializer(serializers.ModelSerializer):
     class Meta:
         model = UserProfile
+        fields = ['uuid', 'pubkey', 'description']
 
 
 class UserSerializer(serializers.HyperlinkedModelSerializer):
     userprofile = UserProfileSerializer()
+    authtokens = serializers.Field()
     
     class Meta:
         model = User
+        exclude = ['groups', 'password', 'user_permissions']

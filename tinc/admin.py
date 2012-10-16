f5d610e10c36bb0e347f4eba990b678b48da7420
Marc Aymerich <confine@controller.confine-project.eu>
2012-10-16 14:26:59 +0200

    Imporved actions

--- tinc/admin.py
+++ tinc/admin.py
@@ -1,4 +1,5 @@
-from common.admin import insert_inline, admin_link, insert_action, get_modeladmin, action_as_view
+from common.admin import (insert_inline, admin_link, insert_action, get_modeladmin, 
+    action_as_view, DynamicChangeViewLinksMixin)
 from django.contrib import admin
 from django.contrib.auth.models import User
 from django.contrib.contenttypes import generic
@@ -42,10 +43,20 @@ class GatewayAdmin(admin.ModelAdmin):
     inlines = [TincServerInline]
 
 
-class HostAdmin(admin.ModelAdmin):
+class HostAdmin(DynamicChangeViewLinksMixin):
     list_display = ['description', 'id', admin_link('admin')]
     inlines = [TincClientInline]
     actions = [set_island]
+    change_view_links = [('set-island', 'set_island_view', 'Set Island', ''),]
+    
+    def get_form(self, request, *args, **kwargs):
+        """ request.user as default host admin """
+        form = super(HostAdmin, self).get_form(request, *args, **kwargs)
+        form.base_fields['admin'].initial = request.user
+        return form
+
+    def set_island_view(modeladmin, request, object_id):
+        return action_as_view(set_island, modeladmin, request, object_id)
 
 
 admin.site.register(Host, HostAdmin)
@@ -67,8 +78,8 @@ insert_action(Node, set_island)
 
 
 # FIXME set_island_view() takes exactly 3 non-keyword arguments (1 given)
-#def set_island_view(modeladmin, request, object_id):
-#    return action_as_view(set_island, modeladmin, request, object_id)
-#
-#node_modeladmin = get_modeladmin(Node)
-#node_modeladmin.set_change_view_link('set-island', set_island_view, 'Set Island', '')
+def set_island_view(modeladmin, request, object_id):
+    return action_as_view(set_island, modeladmin, request, object_id)
+
+node_modeladmin = get_modeladmin(Node)
+node_modeladmin.set_change_view_link('set-island', set_island_view, 'Set Island', '')

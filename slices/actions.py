806b532e27f048957ed0c6d575ea625d31284fb2
Marc Aymerich <confine@controller.confine-project.eu>
2012-10-17 16:13:06 +0200

    Small fixes

--- slices/actions.py
+++ slices/actions.py
@@ -1,6 +1,7 @@
 from django.contrib import messages
 from django.db import router, transaction
 from slices import settings
+from django.utils.translation import ugettext_lazy, ugettext as _
 
 
 @transaction.commit_on_success
@@ -19,4 +20,4 @@ def reset_selected(modeladmin, request, queryset):
         modeladmin.log_change(request, obj, "Instructed to reset")
     msg = "%s selected has been reseted" % (obj._meta.verbose_name_plural, queryset.count())
     modeladmin.message_user(request, msg)
-
+reset_selected.short_description = ugettext_lazy("Reset selected %(verbose_name_plural)s")

dcd1008b0fa0b9f8c3f42df64c78c2784d927c04
Marc Aymerich <confine@controller.confine-project.eu>
2012-10-17 17:48:05 +0200

    Removed save and continue

--- slices/admin.py
+++ slices/admin.py
@@ -83,6 +83,7 @@ class NodeListAdmin(admin.ModelAdmin):
 class AddSliverAdmin(SliverAdmin):
     fields = ['description']
     add_form_template = 'admin/slices/slice/add_sliver.html'
+    show_save_and_continue = False
     
     def add_view(self, request, slice_id, node_id, form_url='', extra_context=None):
         self.slice_id = slice_id

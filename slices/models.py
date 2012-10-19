2c82a21737c438b2e3f3ea7b3e872df25677fb61
Marc Aymerich <confine@controller.confine-project.eu>
2012-10-19 15:26:45 +0200

    Changed IsolatedIface model

--- slices/models.py
+++ slices/models.py
@@ -176,7 +176,7 @@ class SliverIface(models.Model):
 
 class IsolatedIface(SliverIface):
     sliver = models.ForeignKey(Sliver)
-    parent = models.ForeignKey('nodes.DirectIface')
+    parent = models.ForeignKey('nodes.DirectIface', unique=True)
     
     class Meta:
         unique_together = ['sliver', 'parent']

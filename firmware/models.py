975d165a4f194e3f3dbc14511a5772e729eb3a57
Marc Aymerich <confine@controller.confine-project.eu>
2012-10-30 18:14:11 +0100

    Added more stuff for firmware generatioin

--- firmware/models.py
+++ firmware/models.py
@@ -1,19 +1,47 @@
 from common.fields import MultiSelectField
+from common.models import generate_chainer_manager
 from django.core.exceptions import ValidationError
 from django.db import models
 from firmware import settings
 from nodes.settings import NODE_ARCHS
 from singleton_models.models import SingletonModel
 
+class QueueQuerySet(models.query.QuerySet):
+    def get_current(self, node):
+        build = FirmwareBuild.objects.get(node=node)
+        config = FirmwareConfig.objects.get()
+        if build.match(config): 
+            return build
+        else: 
+            raise FirmwareBuild.DoesNotExist()
+
 
 class FirmwareBuild(models.Model):
-    node = models.ForeignKey('nodes.Node')
+    node = models.OneToOneField('nodes.Node')
     date = models.DateTimeField(auto_now_add=True)
     version = models.CharField(max_length=64)
     image = models.FileField(upload_to=settings.FIRMWARE_DIR)
     
+    objects = generate_chainer_manager(QueueQuerySet)
+    
     def __unicode__(self):
         return self.node.description
+    
+    @classmethod
+    def build(cls, node):
+        FirmwareConfig.objects.get().build(node)
+    
+    def get_uci(self):
+        return self.firmwarebuilduci_set.all()
+    
+    def match(self, config):
+        if self.version != config.version: 
+            return False
+        ucis = set(self.get_uci().values_list('section', 'option', 'value'))
+        get = lambda u: (u.section, u.option, u.get_value(self.node))
+        config_ucis = set(map(get, config.get_uci()))
+        return ucis == config_ucis
+
 
 class FirmwareConfig(SingletonModel):
     description = models.CharField(max_length=255)
@@ -27,27 +55,30 @@ class FirmwareConfig(SingletonModel):
     def __unicode__(self):
         return 'Current Firmware Config'
     
+    def get_uci(self):
+        return self.firmwareconfiguci_set.all()
+    
     def build(self, node):
         # TODO how to get public_ipv4_avail ?
         from confw import confw
-        arch_regex = "(^|\s)%s(,|$)" % node.arch
+        arch_regex = "(^|,)%s(,|$)" % node.arch
         base_image = BaseImage.objects.get(architectures__regex=arch_regex).image
-        template = confw.template('generic', 'confine', basedir='/tmp/templates/')
-        files = confw.files('generic', basedir='/tmp/files/')
+#        template = confw.template('generic', 'confine', basedir='/tmp/templates/')
+#        files = confw.files('generic', basedir='/tmp/files/')
         build_uci = []
-        for uci in self.firmwareconfiguci_set.all():
-            value = eval(uci.value)
-            template.set(uci.section, uci.option, value)
+        for uci in self.get_uci():
+            value = uci.get_value(node)
+#            template.set(uci.section, uci.option, value)
             build_uci.append({'section': uci.section, 
                 'option': uci.option, 
                 'value': value})
-        image = confw.image(template, files)
-        image.build(base_image.path, gzip=True)
-        image.clean()
+#        image = confw.image(template, files)
+#        image.build(base_image.path, gzip=True)
+#        image.clean()
         # TODO iamge.path
         # Create FirmwareBuild object
         build = FirmwareBuild.objects.create(node=node, version=self.version,
-            image=image.path)
+            image='firmwares/openwrt-x86-generic-combined-ext4-40.img.gz')
         for uci in build_uci:
             FirmwareBuildUCI.objects.create(build=build, **uci)
 
@@ -86,6 +117,9 @@ class FirmwareConfigUCI(models.Model):
     
     def __unicode__(self):
         return "%s.%s" % (self.section, self.option)
+    
+    def get_value(self, node):
+        return unicode(eval(self.value))
 
 
 class FirmwareBuildUCI(models.Model):
@@ -93,4 +127,7 @@ class FirmwareBuildUCI(models.Model):
     section = models.CharField(max_length=32, help_text="UCI config statement")
     option = models.CharField(max_length=32, help_text="UCI option statement")
     value = models.CharField(max_length=255)
+    
+    class Meta:
+        unique_together = ['build', 'section', 'option']
 

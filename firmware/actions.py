1c93369fb0eaf2c6493c10230a31738352b01377
Marc Aymerich <confine@controller.confine-project.eu>
2012-11-07 18:07:55 +0100

    Cleaned firmware code

--- firmware/actions.py
+++ firmware/actions.py
@@ -1,12 +1,13 @@
-from django.core.exceptions import PermissionDenied
 from django.contrib import messages
 from django.contrib.admin import helpers
+from django.core.exceptions import PermissionDenied
+from django.core.urlresolvers import reverse
 from django.db import router, transaction
 from django.template.response import TemplateResponse
 from django.utils.encoding import force_text
 from django.utils.safestring import mark_safe
-from django.utils.translation import ugettext_lazy, ugettext as _
-from firmware.models import Build, Config
+from django.utils.translation import ugettext_lazy
+from firmware.models import Build
 
 
 @transaction.commit_on_success
@@ -38,7 +39,6 @@ def get_firmware(modeladmin, request, queryset):
         'action_checkbox_name': helpers.ACTION_CHECKBOX_NAME,
         'node': node,
     }
-    from django.core.urlresolvers import reverse
 
     node_url = reverse("admin:nodes_node_change", args=[node.pk])
     node_link = '<a href="%s">%s</a>' % (node_url, node.description)
@@ -54,26 +54,24 @@ def get_firmware(modeladmin, request, queryset):
                     this research device, but you can instruct the system to build a 
                     fresh one for you, it will take only a few seconds. 
                     <p> Do you want me to build a new firwmare?</p>"""),
-                "allow_build": True,
             })
             return TemplateResponse(request, 'admin/get_firmware.html', context, 
                 current_app=modeladmin.admin_site.name)
     
-    description_allow_build = {
-        Build.REQUESTED: ["Building request received.", False],
-        Build.QUEUED: ["Building task queued for building.", False],
-        Build.BUILDING: ["Building...", False],
-        Build.AVAILABLE: ["Firmware available for download.", False],
-        Build.DELETED: ["""This firmware is no longer available. Do you want to 
-            build it again?""", True],
-        Build.FAILED: ["The last building has failed. Do you want to try again?", True]
+    description = {
+        Build.REQUESTED: "Building request received.",
+        Build.QUEUED: "Building task queued for building.",
+        Build.BUILDING: "Building...",
+        Build.AVAILABLE: "Firmware available for download.",
+        Build.DELETED: """This firmware is no longer available. Do you want to 
+            build it again?""",
+        Build.FAILED: "The last building has failed. Do you want to try again?",
     }
     context.update({
         "title": "Research Device Firmware for '%s'" % node.description,
         "content_title": mark_safe("Research Device Firmware for '%s'" % node_link),
-        "content_message": description_allow_build[build.state][0],
+        "content_message": description[build.state],
         "build": build,
-        "allow_build": description_allow_build[build.state][1],
     })
     
     # Display the confirmation page

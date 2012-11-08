cebfbec6000b5b871bbf37eb593ef3d214b4932f
glic3rinu <marcay@pangea.org>
2012-11-08 12:33:59 +0100

    Moved media_private to private

--- controller/urls.py
+++ controller/urls.py
@@ -8,7 +8,7 @@ api.autodiscover()
 urlpatterns = patterns('',
     url(r'^admin/', include(admin.site.urls)),
     url(r'^admin_tools/', include('admin_tools.urls')),
-    url(r'^media_private/', include('private_files.urls')),
+    url(r'^private/', include('private_files.urls')),
     url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
     url(r'^api/', include(api.urls)),
 )

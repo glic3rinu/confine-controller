830d48e12eb4403cd404bb8f5b7ffb992e1507fd
Marc Aymerich <confine@controller.confine-project.eu>
2012-11-09 19:51:52 +0100

    Improved Browsable api description with links

--- nodes/api.py
+++ nodes/api.py
@@ -8,10 +8,12 @@ from rest_framework import generics
 
 class NodeList(generics.ListCreateAPIView):
     """ 
-    **Media type:** `application/vnd.confine.server.NodeList.v0+json`
+    **Media type:** [`application/vnd.confine.server.NodeList.v0+json`](http://
+    wiki.confine-project.eu/arch:rest-api?&#nodelist_at_server)
     
-    This resource lists the nodes available in the testbed and provides API URIs
-    to navigate to them.
+    This resource lists the [nodes](http://wiki.confine-project.eu/arch:rest-
+    api?&#node_at_server) available in the testbed and provides API URIs to 
+    navigate to them.
     """
     model = Node
     serializer_class = NodeSerializer
@@ -20,10 +22,12 @@ class NodeList(generics.ListCreateAPIView):
 
 class NodeDetail(generics.RetrieveUpdateDestroyAPIView):
     """
-    **Media type:** `application/vnd.confine.server.Node.v0+json`
+    **Media type:** [`application/vnd.confine.server.Node.v0+json`](http://
+    wiki.confine-project.eu/arch:rest-api?&#node_at_server)
     
     This resource describes a node in the testbed as well as listing the 
-    slivers intended to run on it with API URIs to navigate to them.
+    [slivers](http://wiki.confine-project.eu/arch:rest-api?&#sliver_at_server)
+    intended to run on it with API URIs to navigate to them.
     """
     model = Node
     serializer_class = NodeSerializer
@@ -31,7 +35,8 @@ class NodeDetail(generics.RetrieveUpdateDestroyAPIView):
 
 class ServerDetail(generics.RetrieveUpdateDestroyAPIView):
     """ 
-    **Media type:** `application/vnd.confine.server.Server.v0+json`
+    **Media type:** [`application/vnd.confine.server.Server.v0+json`](http://
+    wiki.confine-project.eu/arch:rest-api?&#server_at_server)
     
     This resource describes the testbed server (controller).
     """

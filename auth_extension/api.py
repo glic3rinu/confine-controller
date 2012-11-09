c9c66c38708aa7025e551e2262f28270ebcc2825
Marc Aymerich <confine@controller.confine-project.eu>
2012-11-09 19:54:28 +0100

    Fixed typo

--- auth_extension/api.py
+++ auth_extension/api.py
@@ -10,7 +10,7 @@ class UserList(generics.ListCreateAPIView):
     wiki.confine-project.eu/arch:rest-api?&#userlist_at_server)
     
     This resource lists the [users](http://wiki.confine-project.eu/arch:rest-api
-    ?&#user_at_server present in the testbed and provides API URIs to navigate 
+    ?&#user_at_server) present in the testbed and provides API URIs to navigate 
     to them.
     """
     model = User

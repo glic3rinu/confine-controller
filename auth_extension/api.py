310eaad1895cdef08a128ee5195d6ac1aaa015f8
glic3rinu <marcay@pangea.org>
2012-11-09 16:25:02 +0100

    Added documentation on the api resources

--- auth_extension/api.py
+++ auth_extension/api.py
@@ -5,12 +5,23 @@ from rest_framework import generics
 
 
 class UserList(generics.ListCreateAPIView):
+    """
+    **Media type:** `application/vnd.confine.server.UserList.v0+json`
+    
+    This resource lists the users present in the testbed and provides API URIs 
+    to navigate to them.
+    """
     model = User
     serializer_class = UserSerializer
     filter_fields = ('username',)
 
 
 class UserDetail(generics.RetrieveUpdateDestroyAPIView):
+    """
+    **Media type:** `application/vnd.confine.server.User.v0+json`
+    
+    This resource describes a person using the testbed.
+    """
     model = User
     serializer_class = UserSerializer
 
--- slices/api.py
+++ slices/api.py
@@ -5,32 +5,69 @@ from rest_framework import generics
 
 
 class SliceList(generics.ListCreateAPIView):
+    """
+    **Media type:** `application/vnd.confine.server.SliceList.v0+json`
+    
+    This resource lists the slices present in the testbed and provides API URIs 
+    to navigate to them.
+    """
     model = Slice
     serializer_class = SliceSerializer
     filter_fields = ('set_state', )
 
 
 class SliceDetail(generics.RetrieveUpdateDestroyAPIView):
+    """
+    **Media type:** `application/vnd.confine.server.Slice.v0+json`
+    
+    This resource describes a slice in the testbed, including its slivers with 
+    API URIs to navigate to them.
+    """
     model = Slice
     serializer_class = SliceSerializer
 
 
 class SliverList(generics.ListCreateAPIView):
+    """
+    **Media type:** `application/vnd.confine.server.SliverList.v0+json`
+    
+    This resource lists the slivers present in the testbed and provides API URIs 
+    to navigate to them.
+    """
+    
     model = Sliver
     serializer_class = SliverSerializer
 
 
 class SliverDetail(generics.RetrieveUpdateDestroyAPIView):
+    """
+    **Media type:** `application/vnd.confine.server.Sliver.v0+json`
+    
+    This resource describes a sliver in the testbed, with API URIs to navigate 
+    to the slice it is part of and the node it is intended to run on.
+    """
     model = Sliver
     serializer_class = SliverSerializer
 
 
 class TemplateList(generics.ListCreateAPIView):
+    """
+    **Media type:** `application/vnd.confine.server.TemplateList.v0+json`
+    
+    This resource lists the sliver templates available in the testbed and 
+    provides API URIs to navigate to them.
+    """
     model = Template
     serializer_class = TemplateSerializer
 
 
 class TemplateDetail(generics.RetrieveUpdateDestroyAPIView):
+    """
+    **Media type:** `application/vnd.confine.server.Template.v0+json`
+    
+    This resource describes a template available in the testbed for slices and 
+    slivers to use.
+    """
     model = Template
     serializer_class = TemplateSerializer
 

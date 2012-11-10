1db178d7f6701ca5d6bd68712eb0c04fd4a32e4f
Marc Aymerich <confine@controller.confine-project.eu>
2012-11-10 13:07:32 +0100

    Added docstrings and improved field descriptions

--- nodes/models.py
+++ nodes/models.py
@@ -8,6 +8,9 @@ from singleton_models.models import SingletonModel
 
 
 class CnHost(models.Model):
+    """
+    Describes a host in the Community Network.
+    """
     cn_url = models.URLField(blank=True)
     cndb_uri = models.CharField(max_length=256, blank=True)
     cndb_cached_on = models.DateTimeField(null=True, blank=True)
@@ -20,6 +23,10 @@ class CnHost(models.Model):
 
 
 class Node(CnHost):
+    """
+    Describes a node in the testbed. 
+    See Node archiiecture: http://wiki.confine-project.eu/arch:node
+    """
     STATES = (('install_conf', 'Install Configuration'),
               ('install_cert', 'Install Certificate'),
               ('debug', 'Debug'),
@@ -27,41 +34,47 @@ class Node(CnHost):
               ('safe', 'Safe'),
               ('production', 'Production'),)
     
-    uuid = fields.UUIDField(auto=True, help_text="""A universally unique 
-        identifier (UUID, RFC 4122).""")
-    pubkey = models.TextField(unique=True, null=True, blank=True, help_text="""A 
-        PEM-encoded RSA public key for this RD (used by SFA).""")
-    cert = models.TextField(unique=True, null=True, blank=True, help_text="""An 
-        X.509 PEM-encoded certificate for this RD. The certificate may be signed 
-        by a CA recognised in the testbed and required by clients and services 
-        accessing the node API.""")
-    description = models.CharField(max_length=256)
-    arch = models.CharField(max_length=16, choices=settings.NODE_ARCHS, 
-        default=settings.DEFAULT_NODE_ARCH, help_text="""Architecture 
-        of this RD (as reported by uname -m).""")
+    uuid = fields.UUIDField(auto=True, 
+        help_text='Universally unique identifier (UUID, RFC 4122).')
+    pubkey = models.TextField(unique=True, null=True, blank=True, 
+        help_text='PEM-encoded RSA public key for this RD (used by SFA).')
+    cert = models.TextField(unique=True, null=True, blank=True, 
+        help_text='X.509 PEM-encoded certificate for this RD. The certificate '
+        'may be signed by a CA recognised in the testbed and required by clients'
+        ' and services accessing the node API.')
+    description = models.CharField(max_length=256,
+        help_text='Optional free-form textual description of this host/device.')
+    arch = models.CharField(verbose_name='Architecture', max_length=16, 
+        choices=settings.NODE_ARCHS, default=settings.DEFAULT_NODE_ARCH, 
+        help_text='Architecture of this RD (as reported by uname -m).',)
     local_iface = models.CharField(verbose_name="Local Interface", max_length=16, 
-        default='eth0', help_text="""Name of the interface used as a local interface. 
-        See <a href="wiki.confine-project.eu/arch:node">node architecture</a>.""")
+        default='eth0', help_text='Name of the interface used as a local '
+        'interface. See <a href="wiki.confine-project.eu/arch:node">node '
+        'architecture</a>.')
     priv_ipv4_prefix = models.GenericIPAddressField(protocol='IPv4', null=True, 
-        blank=True, help_text="""IPv4 /24 network in CIDR notation used as a node 
-        private IPv4 prefix. See <a href="http://wiki.confine-project.eu/arch:addressing">
-        addressing</a> for legal values. %s When null.""" % settings.PRIV_IPV4_PREFIX_DFLT)
+        blank=True, verbose_name='Private IPv4 Prefix', help_text='IPv4 /24 '
+        'network in CIDR notation used as a node private IPv4 prefix. See <a '
+        'href="http://wiki.confine-project.eu/arch:addressing">addressing</a> '
+        'for legal values. %s When null.' % settings.PRIV_IPV4_PREFIX_DFLT)
     # TODO restric this according the data model
     sliver_mac_prefix = models.PositiveSmallIntegerField(max_length=16, null=True,
-        blank=True, help_text="""A 16-bit integer number in 0x-prefixed hexadecimal 
-        notation used as the node sliver MAC prefix. See 
-        <a href="http://wiki.confine-project.eu/arch:addressing">addressing</a> 
-        for legal values. %s when null.</a>.""" % settings.SLIVER_MAC_PREFIX_DFLT)
-    sliver_pub_ipv4_total = models.IntegerField(default=0, help_text="""Total number 
-        of public (from the point of view of the CN) IPv4 addresses available in 
-        this node's local network to be allocated to slivers' public interfaces (see 
-        <a href="http://wiki.confine-project.eu/arch:node">node</a> architecture). 
-        If the local network uses private addresses the value should be 0.""")
-    boot_sn = models.IntegerField(default=0, blank=True, help_text="""Number of 
-        times this RD has been instructed to be rebooted.""")
-    set_state = models.CharField(max_length=16, choices=STATES, default='install_conf')
-    admin = models.ForeignKey(User, help_text="""The user who administrates this 
-        node (its creator by default).""")
+        blank=True, verbose_name='Sliver MAC Prefix', help_text='A 16-bit '
+        'integer number in 0x-prefixed hexadecimal notation used as the node '
+        'sliver MAC prefix. See <a href="http://wiki.confine-project.eu/arch:'
+        'addressing">addressing</a> for legal values. %s when null.</a>.' 
+        % settings.SLIVER_MAC_PREFIX_DFLT)
+    sliver_pub_ipv4_total = models.IntegerField(default=0, verbose_name='Sliver'
+        ' Public IPv4 Total', help_text='Total number of public (from the point '
+        'of view of the CN) IPv4 addresses available in this node\'s local '
+        'network to be allocated to slivers\' public interfaces (see <a href="'
+        'http://wiki.confine-project.eu/arch:node">node</a> architecture). If '
+        'the local network uses private addresses the value should be 0.')
+    boot_sn = models.IntegerField(default=0, blank=True, help_text='Number of '
+        'times this RD has been instructed to be rebooted.')
+    set_state = models.CharField(max_length=16, choices=STATES, 
+        default='install_conf')
+    admin = models.ForeignKey(User, help_text='User who administrates this node'
+        ' (its creator by default).')
     
     def __unicode__(self):
         return self.description
@@ -114,6 +127,10 @@ class Node(CnHost):
 
 
 class NodeProp(models.Model):
+    """ 
+    A mapping of (non-empty) arbitrary node property names to their (string) 
+    values.
+    """
     node = models.ForeignKey(Node)
     name = models.CharField(max_length=32)
     value = models.CharField(max_length=256)
@@ -126,6 +143,10 @@ class NodeProp(models.Model):
 
 
 class DirectIface(models.Model):
+    """
+    Interfaces used as direct interfaces. 
+    See node architecture: http://wiki.confine-project.eu/arch:node
+    """
     name = models.CharField(max_length=16)
     node = models.ForeignKey(Node)
     
@@ -137,6 +158,9 @@ class DirectIface(models.Model):
 
 
 class Server(SingletonModel, CnHost):
+    """
+    Describes the testbed server (controller).
+    """
     class Meta:
         verbose_name = "server"
         verbose_name_plural = "server"

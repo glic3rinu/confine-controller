64813f9a1dbcbe5ad233a3dc00fa19f37b685320
Marc Aymerich <confine@controller.confine-project.eu>
2012-11-11 19:20:21 +0100

    Removed ADMIN_MEDIA_PREFIX on settings_example.py

--- controller/settings_example.py
+++ controller/settings_example.py
@@ -56,12 +56,6 @@ MEDIA_ROOT = os.path.join(SITE_ROOT, 'media')
 # Examples: "http://media.lawrence.com/media/", "http://example.com/media/"
 MEDIA_URL = '/media/'
 
-# URL prefix for admin media -- CSS, JavaScript and images.
-# Make sure to use a trailing slash.
-# Examples: "http://foo.com/static/admin/", "/static/admin/".
-#TODO: admin_tools keep reliyng on it (delete when solved)
-ADMIN_MEDIA_PREFIX = '/static/admin/'
-
 # Absolute path to the directory static files should be collected to.
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.

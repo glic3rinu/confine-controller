7bf7bb8c6451e20a2f1347acb2cf586e86f9e89d
Marc Aymerich <confine@controller.confine-project.eu>
2012-11-12 14:43:20 +0100

    Fixed slices.admin.nodelist and .addsliverslice support for django 1.5

--- slices/templatetags/slices_modify.py
+++ slices/templatetags/slices_modify.py
@@ -1,6 +1,8 @@
 from django import template
 
+
 register = template.Library()
+
 
 @register.inclusion_tag('admin/submit_line.html', takes_context=True)
 def no_show_save_and_continue(context):
@@ -11,11 +13,12 @@ def no_show_save_and_continue(context):
     change = context['change']
     is_popup = context['is_popup']
     save_as = context['save_as']
-    return {
+    ctx = {
+        'opts': opts,
         'onclick_attrib': (opts.get_ordered_objects() and change
                             and 'onclick="submitOrderForm();"' or ''),
         'show_delete_link': (not is_popup and context['has_delete_permission']
-                              and (change or context['show_delete'])),
+                              and change and context.get('show_delete', True)),
         'show_save_as_new': not is_popup and change and save_as,
         'show_save_and_add_another': context['has_add_permission'] and
                             not is_popup and (not save_as or context['add']),
@@ -23,6 +26,9 @@ def no_show_save_and_continue(context):
         'is_popup': is_popup,
         'show_save': True
     }
+    if context.get('original') is not None:
+        ctx['original'] = context['original']
+    return ctx
 
 
 @register.inclusion_tag('admin/submit_line.html', takes_context=True)
@@ -34,15 +40,20 @@ def only_show_save(context):
     change = context['change']
     is_popup = context['is_popup']
     save_as = context['save_as']
-    return {
+    ctx = {
+        'opts': opts,
         'onclick_attrib': (opts.get_ordered_objects() and change
                             and 'onclick="submitOrderForm();"' or ''),
         'show_delete_link': (not is_popup and context['has_delete_permission']
-                              and (change or context['show_delete'])),
+                              and change and context.get('show_delete', True)),
         'show_save_as_new': not is_popup and change and save_as,
         'show_save_and_add_another': False,
         'show_save_and_continue': False,
         'is_popup': is_popup,
         'show_save': True
     }
+    if context.get('original') is not None:
+        ctx['original'] = context['original']
+    return ctx
+
 

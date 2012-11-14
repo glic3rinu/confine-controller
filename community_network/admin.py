9c2cc052fc4c5f556ecf84cd0ba203f10ce2c03e
confine <confine@ctrl.community-lab.net>
2012-11-14 21:47:25 +0400

    Fixed issues

--- community_network/admin.py
+++ community_network/admin.py
@@ -4,25 +4,25 @@ from django.contrib.contenttypes import generic
 
 from common.admin import insert_inline, insert_list_display, link
 from nodes.models import Server, Node
+from .models import CnHost
 
 
 class CnHostInline(generic.GenericTabularInline):
-    model = TincClient
+    model = CnHost
     max_num = 1
-    fields = ['app_url', 'cndb_uri', 'cndb_cached_on']
-    readonly_fields = ['cndb_cached_on']
+    fields = ['app_url', 'cndb_uri', 'cndb_cached']
+    readonly_fields = ['cndb_cached']
     
-    def cndb_cached_on(self, instance):
+    def cndb_cached(self, instance):
         date = instance.cndb_cached_on
         if not date: return 'Never'
         return date
-    cndb_cached_on.short_description=CnHost._meta.get_field_by_name('cndb_cached_on')[0].verbose_name
-
+    cndb_cached.short_description=CnHost._meta.get_field_by_name('cndb_cached_on')[0].verbose_name
 
 
 # Monkey-Patching Section
 
-app_url_link = link('cnhost__app_url', description='CN URL')
+app_url_link = link('cn__app_url', description='CN URL')
 
 insert_inline(Node, CnHostInline)
 insert_inline(Server, CnHostInline)
@@ -30,5 +30,5 @@ insert_list_display(Node, app_url_link)
 
 if 'tinc' in project_settings.INSTALLED_APPS:
     from tinc.models import Gateway
-    insert_action(Gateway, CnHostInline)
+    insert_inline(Gateway, CnHostInline)
     insert_list_display(Gateway, app_url_link)

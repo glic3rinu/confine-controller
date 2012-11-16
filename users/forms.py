1a860f7e2863fa9400a1efe127667ffd6f6a2dd8
Marc Aymerich <marcay@pangea.org>
2012-11-16 18:10:28 +0400

    More improvements

--- users/forms.py
+++ users/forms.py
@@ -1,7 +1,7 @@
 from django import forms
 from django.contrib.auth.forms import ReadOnlyPasswordHashField
 
-from users.models import User
+from .models import User
 
 
 class UserCreationForm(forms.ModelForm):

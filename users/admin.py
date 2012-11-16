b855db79f118a5fc960d713a31061908ca834b96
Marc Aymerich <marcay@pangea.org>
2012-11-16 15:08:28 +0400

    Added support for custom User and permissions

---
+++ users/admin.py
@@ -0,0 +1,48 @@
+from django import forms
+from django.contrib import admin
+from django.contrib.auth.models import Group
+from django.contrib.auth.admin import UserAdmin
+
+from users.forms import UserCreationForm, UserChangeForm
+from users.models import User, AuthToken, Role, Permission
+
+
+class AuthTokenInline(admin.TabularInline):
+    model = AuthToken
+    extra = 0
+
+
+class RoleAdmin(admin.ModelAdmin):
+    filter_horizontal = ['permissions']
+
+
+class UserAdmin(UserAdmin):
+    list_display = ('username', 'email', 'first_name', 'last_name', 'is_superuser',
+                    'is_active', )
+    list_filter = ('is_superuser', 'is_active', 'research_groups')
+    fieldsets = (
+        (None, {'fields': ('username', 'password')}),
+        ('Personal info', {'fields': ('first_name', 'last_name', 'email', 'phone',
+                                      'description',)}),
+        ('Permissions', {'fields': ('is_active', 'is_superuser',)}),
+        ('Important dates', {'fields': ('last_login', 'date_joined')}),
+        ('SFA', {'fields': ('uuid', 'pubkey')}),
+        )
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('username', 'password1', 'password2')}
+        ),)
+    
+    search_fields = ('username', 'email', 'first_name', 'last_name')
+    inlines = [AuthTokenInline]
+    filter_horizontal = ()
+    form = UserChangeForm
+    add_form = UserCreationForm
+
+
+admin.site.register(Permission)
+admin.site.register(Role, RoleAdmin)
+admin.site.register(User, UserAdmin)
+admin.site.unregister(Group)
+
---
+++ users/models.py
@@ -0,0 +1,183 @@
+import re
+
+from django_extensions.db.fields import UUIDField
+from django.contrib.auth.models import BaseUserManager, AbstractBaseUser
+from django.contrib.contenttypes.models import ContentType
+from django.core import validators
+from django.db import models
+from django.utils import timezone
+
+
+class Permission(models.Model):
+    ACTIONS = (
+        ('view', 'View'),
+        ('add', 'Add'),
+        ('change', 'Change'),
+        ('delete', 'Delete'),)
+    
+    content_type = models.ForeignKey(ContentType, related_name='testbedpermission_set')
+    action = models.CharField(max_length=16, choices=ACTIONS)
+    evaluation = models.CharField(max_length=256)
+
+
+class Role(models.Model):
+    name = models.CharField(max_length=32)
+    permissions = models.ManyToManyField(Permission)
+
+
+class ResearchGroup(models.Model):
+    name = models.CharField(max_length=32, unique=True)
+    description = models.CharField(max_length=256, blank=True)
+    address = models.TextField()
+    city = models.CharField(max_length=32)
+    state = models.CharField(max_length=32)
+    postal_code = models.PositiveIntegerField(blank=True, null=True)
+    country = models.CharField(max_length=32) 
+    url = models.URLField(blank=True)
+    
+    def __unicode__(self):
+        return self.name
+
+
+class UserResearchGroup(models.Model):
+    user = models.ForeignKey('users.User')
+    research_group = models.ForeignKey(ResearchGroup)
+    role = models.ForeignKey(Role)
+
+
+class UserManager(BaseUserManager):
+    def create_user(self, username, email=None, password=None, **extra_fields):
+        """
+        Creates and saves a User with the given username, email and password.
+        """
+        now = timezone.now()
+        if not username:
+            raise ValueError('The given username must be set')
+        email = UserManager.normalize_email(email)
+        user = self.model(username=username, email=email, is_active=True, 
+                          is_superuser=False, last_login=now, date_joined=now, 
+                          **extra_fields)
+        
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+    
+    def create_superuser(self, username, email, password, **extra_fields):
+        u = self.create_user(username, email, password, **extra_fields)
+        u.is_active = True
+        u.is_superuser = True
+        u.save(using=self._db)
+        return u
+
+
+class User(AbstractBaseUser):
+    """
+    Describes a person using the testbed.
+    
+    The implementation is based on auth.models.AbstractBaseUser, more:
+    https://docs.djangoproject.com/en/dev/topics/auth/#customizing-the-user-model
+    """
+    username = models.CharField(max_length=30, unique=True,
+        help_text='Required. 30 characters or fewer. Letters, numbers and '
+                  '@/./+/-/_ characters',
+        validators=[validators.RegexValidator(re.compile('^[\w.@+-]+$'), 
+                    'Enter a valid username.', 'invalid')])
+    email = models.EmailField('Email Address', max_length=255, unique=True)
+    description = models.TextField(blank=True, 
+        help_text='An optional free-form textual description of this user, it '
+                  'can include URLs and other information.')
+    first_name = models.CharField(max_length=30, blank=True)
+    last_name = models.CharField(max_length=30, blank=True)
+    phone = models.CharField(max_length=30, blank=True)
+    is_active = models.BooleanField(default=True,
+        help_text='Designates whether this user should be treated as '
+                  'active. Unselect this instead of deleting accounts.')
+    is_superuser = models.BooleanField(default=False,
+        help_text='Designates that this user has all permissions without '
+                    'explicitly assigning them.')
+    date_joined = models.DateTimeField(default=timezone.now)
+    pubkey = models.TextField('Public Key', unique=True, null=True, blank=True, 
+        help_text='A PEM-encoded RSA public key for this user (used by SFA).')
+    uuid = models.CharField(max_length=36, unique=True, blank=True,
+        help_text='A universally unique identifier (UUID, RFC 4122) for this '
+                  'user (used by SFA). This is optional, but once set to a valid '
+                  'UUID it can not be changed.')
+    research_groups = models.ManyToManyField(ResearchGroup, blank=True, 
+        through=UserResearchGroup)
+    
+    objects = UserManager()
+    
+    USERNAME_FIELD = 'username'
+    REQUIRED_FIELDS = ['email']
+    
+    class Meta:
+        verbose_name = 'user'
+        verbose_name_plural = 'users'
+    
+    def __unicode__(self):
+        return self.username
+    
+    def clean(self):
+        """ 
+        Empty pubkey as NULL instead of empty string 
+        """
+        if self.pubkey is u'': self.pubkey = None
+        super(TestbedUser, self).clean()
+    
+    def get_full_name(self):
+        full_name = '%s %s' % (self.first_name, self.last_name)
+        return full_name.strip()
+    
+    def get_short_name(self):
+        return self.first_name
+    
+    def has_perm(self, perm, obj=None):
+        #TODO implemet
+        """
+        Returns True if the user has the specified permission. This method
+        queries all available auth backends, but returns immediately if any
+        backend returns True. 
+        """
+        if self.is_active and self.is_superuser:
+            return True
+        return self.is_active
+    
+    def has_perms(self, perm_list, obj=None):
+        """
+        Returns True if the user has each of the specified permissions. If
+        object is passed, it checks if the user has all required perms for this
+        object.
+        """
+        for perm in perm_list:
+            if not self.has_perm(perm, obj):
+                return False
+        return True
+    
+    def has_module_perms(self, app_label):
+        #TODO implemet
+        """
+        Returns True if the user has any permissions in the given app label.
+        Uses pretty much the same logic as has_perm, above.
+        """
+        if self.is_active and self.is_superuser:
+            return True
+        return self.is_active
+    
+    @property
+    def is_staff(self):
+        """ All users can loggin to the admin interface """
+        return True
+
+
+class AuthToken(models.Model):
+    """ 
+    A list of authentication tokens like SSH or other kinds of public keys or 
+    X.509 certificates to be used for slivers or experiments. The exact valid 
+    format depends on the type of token as long as it is non-empty and only 
+    contains ASCII characters. (e.g. by using PEM encoding or other ASCII armour).
+    """
+    user = models.ForeignKey(User)
+    data = models.CharField(max_length=256)
+    
+    def __unicode__(self):
+        return str(self.pk)

afa17fe104952c1d99915dc80aa3730eabd6a9e6
Marc Aymerich <confine@controller.confine-project.eu>
2012-11-19 23:24:48 +0100

    Added HyperlinkedFileField

--- common/serializers.py
+++ common/serializers.py
@@ -7,3 +7,10 @@ class UriHyperlinkedModelSerializer(serializers.HyperlinkedModelSerializer):
     def __init__(self, *args, **kwargs):
         super(UriHyperlinkedModelSerializer, self).__init__(*args, **kwargs)
         self.fields.pop('url')
+
+
+class HyperlinkedFileField(serializers.FileField):
+    def to_native(self, value):
+        request = self.context.get('request', None)
+        return request.build_absolute_uri(value.url)
+

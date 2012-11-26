b645ccdd6302a05f2535bb27f824a3cae9350ea6
Marc Aymerich <marcay@pangea.org>
2012-11-26 21:59:56 +0400

    Added groups on api

--- users/serializers.py
+++ users/serializers.py
@@ -1,13 +1,25 @@
+from rest_framework import serializers
+
 from common.serializers import UriHyperlinkedModelSerializer
-from users.models import User, Group
+from users.models import User, Group, Roles
+
+
+class RolesSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Roles
+        exclude = ['user', 'id']
 
 
 class UserSerializer(UriHyperlinkedModelSerializer):
+    group_roles = RolesSerializer(source='roles_set')
+    
     class Meta:
         model = User
-        exclude = ['password']
+        exclude = ['password', 'groups']
 
 
 class GroupSerializer(UriHyperlinkedModelSerializer):
+    user_roles = RolesSerializer(source='roles_set')
+    
     class Meta:
         model = Group

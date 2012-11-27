f15fc953e34e0046bb013def1c8c3322a24524b2
Marc Aymerich <marcay@pangea.org>
2012-11-27 16:19:12 +0400

    Added old migrations

---
+++ firmware/migrations/0001_initial.py
@@ -0,0 +1,218 @@
+# -*- coding: utf-8 -*-
+import datetime
+from south.db import db
+from south.v2 import SchemaMigration
+from django.db import models
+
+
+class Migration(SchemaMigration):
+
+    def forwards(self, orm):
+        # Adding model 'Build'
+        db.create_table('firmware_build', (
+            ('id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
+            ('node', self.gf('django.db.models.fields.related.OneToOneField')(to=orm['nodes.Node'], unique=True)),
+            ('date', self.gf('django.db.models.fields.DateTimeField')(auto_now_add=True, blank=True)),
+            ('version', self.gf('django.db.models.fields.CharField')(max_length=64)),
+            ('image', self.gf('private_files.models.fields.PrivateFileField')(max_length=100)),
+            ('task_id', self.gf('django.db.models.fields.CharField')(unique=True, max_length=36)),
+        ))
+        db.send_create_signal('firmware', ['Build'])
+
+        # Adding model 'BuildUCI'
+        db.create_table('firmware_builduci', (
+            ('id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
+            ('build', self.gf('django.db.models.fields.related.ForeignKey')(to=orm['firmware.Build'])),
+            ('section', self.gf('django.db.models.fields.CharField')(max_length=32)),
+            ('option', self.gf('django.db.models.fields.CharField')(max_length=32)),
+            ('value', self.gf('django.db.models.fields.CharField')(max_length=255)),
+        ))
+        db.send_create_signal('firmware', ['BuildUCI'])
+
+        # Adding unique constraint on 'BuildUCI', fields ['build', 'section', 'option']
+        db.create_unique('firmware_builduci', ['build_id', 'section', 'option'])
+
+        # Adding model 'Config'
+        db.create_table('firmware_config', (
+            ('id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
+            ('description', self.gf('django.db.models.fields.CharField')(max_length=255)),
+            ('version', self.gf('django.db.models.fields.CharField')(max_length=64)),
+            ('date', self.gf('django.db.models.fields.DateTimeField')(auto_now=True, blank=True)),
+        ))
+        db.send_create_signal('firmware', ['Config'])
+
+        # Adding model 'BaseImage'
+        db.create_table('firmware_baseimage', (
+            ('id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
+            ('config', self.gf('django.db.models.fields.related.ForeignKey')(to=orm['firmware.Config'])),
+            ('architectures', self.gf('common.fields.MultiSelectField')(max_length=250)),
+            ('image', self.gf('django.db.models.fields.files.FileField')(max_length=100)),
+        ))
+        db.send_create_signal('firmware', ['BaseImage'])
+
+        # Adding model 'ConfigUCI'
+        db.create_table('firmware_configuci', (
+            ('id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
+            ('config', self.gf('django.db.models.fields.related.ForeignKey')(to=orm['firmware.Config'])),
+            ('section', self.gf('django.db.models.fields.CharField')(default='node', max_length=32)),
+            ('option', self.gf('django.db.models.fields.CharField')(max_length=32)),
+            ('value', self.gf('django.db.models.fields.CharField')(max_length=255)),
+        ))
+        db.send_create_signal('firmware', ['ConfigUCI'])
+
+        # Adding unique constraint on 'ConfigUCI', fields ['config', 'section', 'option']
+        db.create_unique('firmware_configuci', ['config_id', 'section', 'option'])
+
+
+    def backwards(self, orm):
+        # Removing unique constraint on 'ConfigUCI', fields ['config', 'section', 'option']
+        db.delete_unique('firmware_configuci', ['config_id', 'section', 'option'])
+
+        # Removing unique constraint on 'BuildUCI', fields ['build', 'section', 'option']
+        db.delete_unique('firmware_builduci', ['build_id', 'section', 'option'])
+
+        # Deleting model 'Build'
+        db.delete_table('firmware_build')
+
+        # Deleting model 'BuildUCI'
+        db.delete_table('firmware_builduci')
+
+        # Deleting model 'Config'
+        db.delete_table('firmware_config')
+
+        # Deleting model 'BaseImage'
+        db.delete_table('firmware_baseimage')
+
+        # Deleting model 'ConfigUCI'
+        db.delete_table('firmware_configuci')
+
+
+    models = {
+        'auth.group': {
+            'Meta': {'object_name': 'Group'},
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '80'}),
+            'permissions': ('django.db.models.fields.related.ManyToManyField', [], {'to': "orm['auth.Permission']", 'symmetrical': 'False', 'blank': 'True'})
+        },
+        'auth.permission': {
+            'Meta': {'ordering': "('content_type__app_label', 'content_type__model', 'codename')", 'unique_together': "(('content_type', 'codename'),)", 'object_name': 'Permission'},
+            'codename': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'content_type': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['contenttypes.ContentType']"}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '50'})
+        },
+        'auth.user': {
+            'Meta': {'object_name': 'User'},
+            'date_joined': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime.now'}),
+            'email': ('django.db.models.fields.EmailField', [], {'max_length': '75', 'blank': 'True'}),
+            'first_name': ('django.db.models.fields.CharField', [], {'max_length': '30', 'blank': 'True'}),
+            'groups': ('django.db.models.fields.related.ManyToManyField', [], {'to': "orm['auth.Group']", 'symmetrical': 'False', 'blank': 'True'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'is_active': ('django.db.models.fields.BooleanField', [], {'default': 'True'}),
+            'is_staff': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'is_superuser': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'last_login': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime.now'}),
+            'last_name': ('django.db.models.fields.CharField', [], {'max_length': '30', 'blank': 'True'}),
+            'password': ('django.db.models.fields.CharField', [], {'max_length': '128'}),
+            'user_permissions': ('django.db.models.fields.related.ManyToManyField', [], {'to': "orm['auth.Permission']", 'symmetrical': 'False', 'blank': 'True'}),
+            'username': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '30'})
+        },
+        'contenttypes.contenttype': {
+            'Meta': {'ordering': "('name',)", 'unique_together': "(('app_label', 'model'),)", 'object_name': 'ContentType', 'db_table': "'django_content_type'"},
+            'app_label': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'model': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '100'})
+        },
+        'firmware.baseimage': {
+            'Meta': {'object_name': 'BaseImage'},
+            'architectures': ('common.fields.MultiSelectField', [], {'max_length': '250'}),
+            'config': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['firmware.Config']"}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'image': ('django.db.models.fields.files.FileField', [], {'max_length': '100'})
+        },
+        'firmware.build': {
+            'Meta': {'ordering': "['-date']", 'object_name': 'Build'},
+            'date': ('django.db.models.fields.DateTimeField', [], {'auto_now_add': 'True', 'blank': 'True'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'image': ('private_files.models.fields.PrivateFileField', [], {'max_length': '100'}),
+            'node': ('django.db.models.fields.related.OneToOneField', [], {'to': "orm['nodes.Node']", 'unique': 'True'}),
+            'task_id': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '36'}),
+            'version': ('django.db.models.fields.CharField', [], {'max_length': '64'})
+        },
+        'firmware.builduci': {
+            'Meta': {'unique_together': "(['build', 'section', 'option'],)", 'object_name': 'BuildUCI'},
+            'build': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['firmware.Build']"}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'option': ('django.db.models.fields.CharField', [], {'max_length': '32'}),
+            'section': ('django.db.models.fields.CharField', [], {'max_length': '32'}),
+            'value': ('django.db.models.fields.CharField', [], {'max_length': '255'})
+        },
+        'firmware.config': {
+            'Meta': {'object_name': 'Config'},
+            'date': ('django.db.models.fields.DateTimeField', [], {'auto_now': 'True', 'blank': 'True'}),
+            'description': ('django.db.models.fields.CharField', [], {'max_length': '255'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'version': ('django.db.models.fields.CharField', [], {'max_length': '64'})
+        },
+        'firmware.configuci': {
+            'Meta': {'unique_together': "(['config', 'section', 'option'],)", 'object_name': 'ConfigUCI'},
+            'config': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['firmware.Config']"}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'option': ('django.db.models.fields.CharField', [], {'max_length': '32'}),
+            'section': ('django.db.models.fields.CharField', [], {'default': "'node'", 'max_length': '32'}),
+            'value': ('django.db.models.fields.CharField', [], {'max_length': '255'})
+        },
+        'nodes.node': {
+            'Meta': {'object_name': 'Node'},
+            'admin': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['auth.User']"}),
+            'arch': ('django.db.models.fields.CharField', [], {'default': "'x86_64'", 'max_length': '16'}),
+            'boot_sn': ('django.db.models.fields.IntegerField', [], {'default': '0', 'blank': 'True'}),
+            'cert': ('django.db.models.fields.TextField', [], {'unique': 'True', 'null': 'True', 'blank': 'True'}),
+            'cn_url': ('django.db.models.fields.URLField', [], {'max_length': '200', 'blank': 'True'}),
+            'cndb_cached_on': ('django.db.models.fields.DateTimeField', [], {'null': 'True', 'blank': 'True'}),
+            'cndb_uri': ('django.db.models.fields.URLField', [], {'max_length': '200', 'blank': 'True'}),
+            'description': ('django.db.models.fields.CharField', [], {'max_length': '256'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'local_iface': ('django.db.models.fields.CharField', [], {'default': "'eth0'", 'max_length': '16'}),
+            'priv_ipv4_prefix': ('django.db.models.fields.GenericIPAddressField', [], {'max_length': '39', 'null': 'True', 'blank': 'True'}),
+            'pubkey': ('django.db.models.fields.TextField', [], {'unique': 'True', 'null': 'True', 'blank': 'True'}),
+            'set_state': ('django.db.models.fields.CharField', [], {'default': "'install_conf'", 'max_length': '16'}),
+            'sliver_mac_prefix': ('django.db.models.fields.PositiveSmallIntegerField', [], {'max_length': '16', 'null': 'True', 'blank': 'True'}),
+            'sliver_pub_ipv4_total': ('django.db.models.fields.IntegerField', [], {'default': '0'}),
+            'uuid': ('django.db.models.fields.CharField', [], {'max_length': '36', 'blank': 'True'})
+        },
+        'tinc.island': {
+            'Meta': {'object_name': 'Island'},
+            'description': ('django.db.models.fields.TextField', [], {'blank': 'True'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '32'})
+        },
+        'tinc.tincaddress': {
+            'Meta': {'object_name': 'TincAddress'},
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'ip_addr': ('django.db.models.fields.GenericIPAddressField', [], {'max_length': '39'}),
+            'island': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['tinc.Island']"}),
+            'port': ('django.db.models.fields.SmallIntegerField', [], {'default': "'666'"}),
+            'server': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['tinc.TincServer']"})
+        },
+        'tinc.tincclient': {
+            'Meta': {'unique_together': "(('content_type', 'object_id'),)", 'object_name': 'TincClient'},
+            'connect_to': ('django.db.models.fields.related.ManyToManyField', [], {'to': "orm['tinc.TincAddress']", 'symmetrical': 'False', 'blank': 'True'}),
+            'content_type': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['contenttypes.ContentType']"}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'island': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['tinc.Island']"}),
+            'object_id': ('django.db.models.fields.PositiveIntegerField', [], {'max_length': '36'}),
+            'pubkey': ('django.db.models.fields.TextField', [], {'unique': 'True'})
+        },
+        'tinc.tincserver': {
+            'Meta': {'unique_together': "(('content_type', 'object_id'),)", 'object_name': 'TincServer'},
+            'connect_to': ('django.db.models.fields.related.ManyToManyField', [], {'to': "orm['tinc.TincAddress']", 'symmetrical': 'False', 'blank': 'True'}),
+            'content_type': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['contenttypes.ContentType']"}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'object_id': ('django.db.models.fields.PositiveIntegerField', [], {'max_length': '36'}),
+            'pubkey': ('django.db.models.fields.TextField', [], {'unique': 'True'})
+        }
+    }
+
+    complete_apps = ['firmware']

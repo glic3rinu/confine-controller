96b4c2513a768aa1f48a38011b940dacfef60841
Marc Aymerich <marcay@pangea.org>
2012-11-28 14:20:54 +0100

    Fixed createsuperuser

--- controller/settings_example.py
+++ controller/settings_example.py
@@ -158,7 +158,7 @@ INSTALLED_APPS = (
     'rest_framework',
 )
 
-AUTH_USER_MODEL = 'users.user'
+AUTH_USER_MODEL = 'users.User'
 
 AUTHENTICATION_BACKENDS = [
     'users.backends.TestbedPermissionBackend',
--- users/__init__.py
+++ users/__init__.py
@@ -1,13 +1,15 @@
 from django.conf import settings
+from django.core.exceptions import ImproperlyConfigured
 from django.utils.importlib import import_module
-
 
 # Autodiscover permissions.py
 def autodiscover():
     """ Auto-discover INSTALLED_APPS permission.py module """
     for app in settings.INSTALLED_APPS:
         mod = import_module(app)
-        try: import_module('%s.permissions' % app)
-        except ImportError: pass
+        try: 
+            import_module('%s.permissions' % app)
+        except (ImportError, ImproperlyConfigured): 
+            pass
 
 autodiscover()

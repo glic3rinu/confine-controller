343c64030801d2cce929b10941b2b4bd6a525931
Marc Aymerich <marcay@pangea.org>
2012-12-05 21:21:15 +0100

    Added suport for IP schema on tinc

---
+++ common/ip.py
@@ -0,0 +1,16 @@
+import re
+
+from IPy import IP
+
+# TODO unificate with slices.utils
+
+def split_len(seq, length):
+    return [seq[i:i+length] for i in range(0, len(seq), length)]
+
+
+def int_to_hex_str(number, digits):
+    return ('%.' + str(digits) + 'x') % number
+
+
+def simplify_ipv6(address):
+    return str(IP(address))

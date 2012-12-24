dc8bdbd1869c511aaa45f01c1f98ef35609123da
Marc Aymerich <marcay@pangea.org>
2012-12-24 15:25:26 +0100

    Improved firmware.image

--- firmware/image.py
+++ firmware/image.py
@@ -3,60 +3,98 @@ from subprocess import Popen, PIPE
 
 
 class Image(object):
-    def __init__(self, base_image):
+    """ 
+    Class-based firmware customization.
+    Based on user-space tools, with partition support.
+    
+    Basic usage:
+        1. Load the base image
+            image = Image('/base/image/file/path.gz')
+        2. Add some files to the base image:
+            image.add_file(file)
+        3. Generate the new image
+            image.build(path='/destination/image/path.gz')
+        4. Clean temporary files created by build() method
+            image.clean()
+    """
+    def __init__(self, base_image, part_nr=2):
         os.stat(base_image)
         self.base_image = base_image
         self.files = []
-        self.base_dir = os.path.dirname(os.path.realpath(__file__))
+        self.part_nr = part_nr
     
     @property
     def image(self):
+        """ image path """
         return os.path.join(self.tmp, "image.bin")
     
     @property
     def mnt(self):
-        return os.path.join(self.tmp, "mnt")
+        """ mnt path """
+        return os.path.join(self.tmp, 'mnt')
     
     @property
-    def tmp_part_file(self):
+    def partition(self):
+        """ partition path """
         return os.path.join(self.tmp, 'part.img')
     
+    @property
+    def scripts(self):
+        """ scripts path """
+        module_path = os.path.dirname(os.path.realpath(__file__))
+        return os.path.join(module_path, 'scripts')
+    
     def prepare(self):
+        """ create temporary directories needed for building the image """
         self.tmp = tempfile.mkdtemp()
         os.mkdir(self.mnt)
     
-    def mount(self, part_nr=2):
-        args = "'%s' %s %s %s" % (self.image, self.mnt, self.tmp_part_file, part_nr)
-        mountimage = "%s/scripts/mountimage.sh -m %s" % (self.base_dir, args)
+    def mount(self):
+        """ mount image partition with user-space tools """
+        args = "'%s' %s %s %s" % (self.image, self.mnt, self.partition, self.part_nr)
+        mountimage = "%s/mountimage.sh -m %s" % (self.scripts, args)
         self._exec_cmd(mountimage)
-
-    def umount(self, part_nr=2):
-        args = "'%s' %s %s %s" % (self.image, self.mnt, self.tmp_part_file, part_nr)
-        umountimage = "%s/scripts/mountimage.sh -u %s" % (self.base_dir, args)
+    
+    def umount(self):
+        """ umount image partition """
+        args = "'%s' %s %s %s" % (self.image, self.mnt, self.partition, self.part_nr)
+        umountimage = "%s/mountimage.sh -u %s" % (self.scripts, args)
         self._exec_cmd(umountimage)
     
     def add_file(self, file):
+        """
+        add files to the image
+        
+        file requires a file.name and file.content attributes
+        and optionally a file.config.mode with chmod-like syntax.
+        """
         self.files.append(file)
     
     def clean(self):
+        """ remove temporary files """
         try: self.umount()
         except: pass
         shutil.rmtree(self.tmp)
     
     def move(self, path):
+        """ move generated image to a destination path """
         shutil.move(self.image, path)
     
     def chmod(self, path, mode):
+        """ change mode of the path """
         chmod = "chmod %s '%s'" % (mode, path) 
         self._exec_cmd(chmod)
     
     def build(self, path=None):
+        """ build the new image """
+        # create temporary dirs
         self.prepare()
+        # extract the image
         extract = "cat '%s' | gunzip -c > '%s'" %(self.base_image, self.image)
         self._exec_cmd(extract)
         self.mount()
         
-        # add files to base_image
+        # create the added files
         for f in self.files:
             dest_path = self.mnt + f.name
             dest_dir = os.path.dirname(dest_path)
@@ -71,21 +109,24 @@ class Image(object):
                 self.chmod(dest_path, f.config.mode)
         
         self.umount()
+        # compress the generated image with gzip
         compress = "gzip " + self.image
         self._exec_cmd(compress)
         self.image += '.gz'
         
+        # move the image to the destination path if required
         if path is not None:
             self.move(path)
     
-    def _exec_cmd(self, command, part_nr=2):
+    def _exec_cmd(self, command):
+        """ Execute shell commands """
         mount_errors = {
-            11: "Unable to find start sector for part %s" % part_nr,
+            11: "Unable to find partition start sector",
             12: "%s already mounted" % self.mnt,
-            13: "Error extracting part %s" % part_nr,
-            14: "Cannot mount extracted part %s" % part_nr,
+            13: "Error extracting partition",
+            14: "Cannot mount extracted partition",
             15: "Unable to unmount %s" % self.mnt,
-            16: "Error joining extracted part %s" % part_nr}
+            16: "Error joining extracted partition"}
         
         cmd = Popen(command, shell=True, stdout=PIPE, stderr=PIPE)
         (stdout, stderr) = cmd.communicate()

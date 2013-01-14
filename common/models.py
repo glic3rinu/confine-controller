d845104e94c53c4bd9fad697c92c3b3592db8766
Marc Aymerich <marcay@pangea.org>
2013-01-14 19:04:13 +0100

    Fixed communitynetwork url bug

--- common/models.py
+++ common/models.py
@@ -24,7 +24,10 @@ def get_field_value(obj, field_name):
     names = field_name.split('__')
     rel = getattr(obj, names.pop(0))
     for name in names:
-        rel = getattr(rel, name)
+        try: rel = getattr(rel, name)
+        except AttributeError:
+            # maybe it is a query manager ...
+            rel = getattr(rel.get(), name)
     return rel
 
 

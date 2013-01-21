05e4088d9ae1c70e273f9a629918de5e4bb84150
Marc Aymerich <marcay@pangea.org>
2013-01-21 21:07:27 +0100

    Bits

--- mgmtnetworks/tinc/backend.py
+++ mgmtnetworks/tinc/backend.py
@@ -2,11 +2,12 @@ class TincBackend(object):
     """ 
     Management Backend class that provides the managemnt address to some testbed components
     """
-    name = 'tinc'
-    
     def address(self, obj):
         """ Relies on obj.tinc.address wich is provided by this same app """
         return obj.tinc.address
-
+    
+    @property
+    def name(self, obj):
+        return obj.tinc._meta.verbose_name.replace(' ', '_')
 
 backend = TincBackend()

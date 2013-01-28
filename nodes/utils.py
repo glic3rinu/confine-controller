a3e330fb03049e12718d2ece8a3fc817017188e1
Marc Aymerich <marcay@pangea.org>
2013-01-28 17:11:21 +0100

    Fixed some API POSTing issues

--- nodes/utils.py
+++ nodes/utils.py
@@ -3,7 +3,7 @@ from django.utils.importlib import import_module
 from . import settings
 
 
-def get_mgmt_backend():
+def get_mgmt_backend(obj):
     mod, inst = settings.NODES_MGMT_BACKEND.rsplit('.', 1)
     mod = import_module(mod)
-    return getattr(mod, inst)
+    return getattr(mod, inst)(obj)

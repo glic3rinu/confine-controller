3e931b75f9425c73c2759459b81e7f66abbb85c4
Marc Aymerich <marcay@pangea.org>
2013-02-04 14:15:25 +0100

    Added some commands

--- apps/common/management/commands/setupapache.py
+++ apps/common/management/commands/setupapache.py
@@ -1,4 +1,4 @@
-from django.conf.settings import SITE_ROOT, PROJECT_ROOT
+from django.conf import settings
 from django.core.management.base import BaseCommand
 
 from common.system import run, check_root
@@ -10,7 +10,7 @@ class Command(BaseCommand):
     @check_root
     def handle(self, *args, **options):
         username = run("ls -dl|awk {'print $3'}")
-        project_name = PROJECT_ROOT.split('/')[-1]
+        project_name = settings.PROJECT_ROOT.split('/')[-1]
         apache_conf = ("WSGIScriptAlias / %(project_root)/wsgi.py\n"
                        "WSGIPythonPath %(site_root)s\n"
                        "<Directory %(project_root)>\n"
@@ -20,13 +20,13 @@ class Command(BaseCommand):
                        "    </Files>\n"
                        "</Directory>\n"
                        "Alias /media/ %(site_root)s/media/\n"
-                       "Alias /static/ %(site_root)s/static/\n" % {'project_root': PROJECT_ROOT,
-                                                                   'site_root': SITE_ROOT})
+                       "Alias /static/ %(site_root)s/static/\n" % {'project_root': settings.PROJECT_ROOT,
+                                                                   'site_root': settings.SITE_ROOT})
         
         run('echo %s > /etc/apache2/sites-available/%s.conf' % (apache_conf, project_name))
         run('a2ensite %s' % project_name)
         # Give upload file permissions to apache
         run('adduser www-data %s' % username)
-        run('chmod g+w %s/media/firmwares' % SITE_ROOT)
-        run('chmod g+w %s/media/templates' % SITE_ROOT)
-        run('chmod g+w %s/private/exp_data' % SITE_ROOT)
+        run('chmod g+w %s/media/firmwares' % settings.SITE_ROOT)
+        run('chmod g+w %s/media/templates' % settings.SITE_ROOT)
+        run('chmod g+w %s/private/exp_data' % settings.SITE_ROOT)

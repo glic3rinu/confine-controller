64140146cde9ae8a9bda26655c9f0879251df27f
Marc Aymerich <marcay@pangea.org>
2013-02-04 16:49:57 +0100

    Added conf

--- setup.py
+++ setup.py
@@ -1,27 +1,18 @@
 import os
 from setuptools import setup, find_packages
 
-packages = find_packages('controller/apps')
-#packages.append('static')
-packages.append('docs')
-
+packages = [
+    'controller',
+]
 
 setup(
     name = 'confine-controller',
     version = '0.1dev',
     packages = packages,
-    package_dir = {
-        '' : 'controller/apps',
- #       'static' : 'static',
-        'docs' : 'docs',
-        'skel' : 'controller/projects/controller',},
     include_package_data = True,
     license = 'BSD License',
     description = 'Django-based framework for building control servers for computer networking and distributed systems testbeds.',
     long_description = 'Django-based framework for building control servers for computer networking and distributed systems testbeds.',
-#    install_requires=[
-#        'kombu==2.4.7',
-#    ],
     scripts=['scripts/controller-admin.sh'],
     url = 'http://wiki.confine-project.eu/soft:server',
     author = 'Marc Aymerich, Santiago Lamora',

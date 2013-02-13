ecce54a105949f6365c9c014a65b9be7aea11568
Marc Aymerich <marcay@pangea.org>
2013-02-13 21:52:13 +0400

    Added version capabilities

--- controller/__init__.py
+++ controller/__init__.py
@@ -1,1 +1,8 @@
+VERSION = (0, 6, 32, 'alpha', 0)
+
+def get_version():
+    # Don't litter controller/__init__.py with all the get_version stuff.
+    # Only import if it's actually called.
+    from django.utils.version import get_version
+    return get_version(version=VERSION)
 

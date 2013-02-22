88b1dc04652f7b2e6dd53a058f23586083107b4f
Marc Aymerich <marcay@pangea.org>
2013-02-22 17:27:17 +0400

    Added stopservices command

--- controller/__init__.py
+++ controller/__init__.py
@@ -1,4 +1,4 @@
-VERSION = (0, 6, 70, 'alpha', 0)
+VERSION = (0, 6, 71, 'alpha', 0)
 
 
 def get_version():
---
+++ controller/management/commands/stopservices.py
@@ -0,0 +1,34 @@
+from optparse import make_option
+
+from django.core.management.base import BaseCommand
+
+from controller.utils.system import run, check_root
+
+
+class Command(BaseCommand):
+    def __init__(self, *args, **kwargs):
+        super(Command, self).__init__(*args, **kwargs)
+        self.option_list = BaseCommand.option_list + (
+            make_option('--no-apache', action='store_false', dest='apache', default=True,
+                help='No apache.'),
+            make_option('--no-tinc', action='store_false', dest='tinc', default=True,
+                help='No tinc.'),
+            make_option('--no-celeryd', action='store_false', dest='celeryd', default=True,
+                help='No celeryd.'),
+            make_option('--no-celeryevcam', action='store_false', dest='celeryevcam', default=True,
+                help='No celeryevcam.'),
+            )
+    
+    option_list = BaseCommand.option_list
+    help = 'stop all related services. Usefull for reload configuration and files.'
+    
+    @check_root
+    def handle(self, *args, **options):
+        if options.get('tinc'):
+            run('service tinc stop')
+        if options.get('apache'):
+            run('service apache2 stop')
+        if options.get('celeryd'):
+            run('service celeryd stop')
+        if options.get('celeryevcam'):
+            run('service celeryevcam stop')

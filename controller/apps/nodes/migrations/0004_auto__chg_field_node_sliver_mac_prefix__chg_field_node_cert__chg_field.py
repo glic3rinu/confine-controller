c36e08b93a32db6e8aa56abc6d11fa1351c664e5
Marc Aymerich <marcay@pangea.org>
2013-03-14 18:23:55 +0400

    Added missing migrations

---
+++ controller/apps/nodes/migrations/0004_auto__chg_field_node_sliver_mac_prefix__chg_field_node_cert__chg_field.py
@@ -0,0 +1,116 @@
+# -*- coding: utf-8 -*-
+import datetime
+from south.db import db
+from south.v2 import SchemaMigration
+from django.db import models
+
+
+class Migration(SchemaMigration):
+
+    def forwards(self, orm):
+
+        # Changing field 'Node.sliver_mac_prefix'
+        db.alter_column(u'nodes_node', 'sliver_mac_prefix', self.gf('controller.models.fields.NullableCharField')(max_length=5, null=True))
+
+        # Changing field 'Node.cert'
+        db.alter_column(u'nodes_node', 'cert', self.gf('controller.models.fields.NullableTextField')(unique=True, null=True))
+
+        # Changing field 'Node.sliver_pub_ipv4_range'
+        db.alter_column(u'nodes_node', 'sliver_pub_ipv4_range', self.gf('controller.models.fields.NullableCharField')(max_length=256, null=True))
+
+    def backwards(self, orm):
+
+        # Changing field 'Node.sliver_mac_prefix'
+        db.alter_column(u'nodes_node', 'sliver_mac_prefix', self.gf('django.db.models.fields.CharField')(max_length=5, null=True))
+
+        # Changing field 'Node.cert'
+        db.alter_column(u'nodes_node', 'cert', self.gf('django.db.models.fields.TextField')(unique=True, null=True))
+
+        # Changing field 'Node.sliver_pub_ipv4_range'
+        db.alter_column(u'nodes_node', 'sliver_pub_ipv4_range', self.gf('django.db.models.fields.CharField')(max_length=256, null=True))
+
+    models = {
+        u'communitynetworks.cnhost': {
+            'Meta': {'unique_together': "(('content_type', 'object_id'),)", 'object_name': 'CnHost'},
+            'app_url': ('django.db.models.fields.URLField', [], {'max_length': '200', 'blank': 'True'}),
+            'cndb_cached_on': ('django.db.models.fields.DateTimeField', [], {'null': 'True', 'blank': 'True'}),
+            'cndb_uri': ('controller.models.fields.URIField', [], {'max_length': '200', 'unique': 'True', 'null': 'True', 'blank': 'True'}),
+            'content_type': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['contenttypes.ContentType']"}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'object_id': ('django.db.models.fields.PositiveIntegerField', [], {})
+        },
+        u'contenttypes.contenttype': {
+            'Meta': {'ordering': "('name',)", 'unique_together': "(('app_label', 'model'),)", 'object_name': 'ContentType', 'db_table': "'django_content_type'"},
+            'app_label': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'model': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '100'})
+        },
+        u'nodes.directiface': {
+            'Meta': {'unique_together': "(['name', 'node'],)", 'object_name': 'DirectIface'},
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '16'}),
+            'node': ('django.db.models.fields.related.ForeignKey', [], {'related_name': "'direct_ifaces'", 'to': u"orm['nodes.Node']"})
+        },
+        u'nodes.node': {
+            'Meta': {'object_name': 'Node'},
+            'arch': ('django.db.models.fields.CharField', [], {'default': "'x86_64'", 'max_length': '16'}),
+            'boot_sn': ('django.db.models.fields.IntegerField', [], {'default': '0', 'blank': 'True'}),
+            'cert': ('controller.models.fields.NullableTextField', [], {'unique': 'True', 'null': 'True', 'blank': 'True'}),
+            'description': ('django.db.models.fields.TextField', [], {'blank': 'True'}),
+            'group': ('django.db.models.fields.related.ForeignKey', [], {'related_name': "'nodes'", 'to': u"orm['users.Group']"}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'local_iface': ('django.db.models.fields.CharField', [], {'default': "'eth0'", 'max_length': '16'}),
+            'name': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '256'}),
+            'priv_ipv4_prefix': ('django.db.models.fields.GenericIPAddressField', [], {'max_length': '39', 'null': 'True', 'blank': 'True'}),
+            'set_state': ('django.db.models.fields.CharField', [], {'default': "'debug'", 'max_length': '16'}),
+            'sliver_mac_prefix': ('controller.models.fields.NullableCharField', [], {'max_length': '5', 'null': 'True', 'blank': 'True'}),
+            'sliver_pub_ipv4': ('django.db.models.fields.CharField', [], {'default': "'none'", 'max_length': '8'}),
+            'sliver_pub_ipv4_range': ('controller.models.fields.NullableCharField', [], {'default': "''", 'max_length': '256', 'null': 'True', 'blank': 'True'}),
+            'sliver_pub_ipv6': ('django.db.models.fields.CharField', [], {'default': "'none'", 'max_length': '8'})
+        },
+        u'nodes.nodeprop': {
+            'Meta': {'unique_together': "(('node', 'name'),)", 'object_name': 'NodeProp'},
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '32'}),
+            'node': ('django.db.models.fields.related.ForeignKey', [], {'related_name': "'properties'", 'to': u"orm['nodes.Node']"}),
+            'value': ('django.db.models.fields.CharField', [], {'max_length': '256'})
+        },
+        u'nodes.server': {
+            'Meta': {'object_name': 'Server'},
+            'description': ('django.db.models.fields.CharField', [], {'max_length': '256'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'})
+        },
+        u'tinc.island': {
+            'Meta': {'object_name': 'Island'},
+            'description': ('django.db.models.fields.TextField', [], {'blank': 'True'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '32'})
+        },
+        u'tinc.tincclient': {
+            'Meta': {'unique_together': "(('content_type', 'object_id'),)", 'object_name': 'TincClient'},
+            'content_type': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['contenttypes.ContentType']"}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'island': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['tinc.Island']", 'null': 'True', 'blank': 'True'}),
+            'object_id': ('django.db.models.fields.PositiveIntegerField', [], {}),
+            'pubkey': ('controller.models.fields.RSAPublicKeyField', [], {'unique': 'True', 'null': 'True', 'blank': 'True'})
+        },
+        u'tinc.tincserver': {
+            'Meta': {'unique_together': "(('content_type', 'object_id'),)", 'object_name': 'TincServer'},
+            'content_type': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['contenttypes.ContentType']"}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'is_active': ('django.db.models.fields.BooleanField', [], {'default': 'True'}),
+            'object_id': ('django.db.models.fields.PositiveIntegerField', [], {}),
+            'pubkey': ('controller.models.fields.RSAPublicKeyField', [], {'unique': 'True', 'null': 'True', 'blank': 'True'})
+        },
+        u'users.group': {
+            'Meta': {'object_name': 'Group'},
+            'allow_nodes': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'allow_slices': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'description': ('django.db.models.fields.TextField', [], {'blank': 'True'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '32'})
+        }
+    }
+
+    complete_apps = ['nodes']

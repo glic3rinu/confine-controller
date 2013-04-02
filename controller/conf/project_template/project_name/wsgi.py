de8e2db4ef611a0e192c3b387dcc0138585a273c
Marc Aymerich <marcay@pangea.org>
2013-04-02 14:14:44 +0400

    Removed sys path from wsgi.py template

--- controller/conf/project_template/project_name/wsgi.py
+++ controller/conf/project_template/project_name/wsgi.py
@@ -7,9 +7,8 @@ For more information on this file, see
 https://docs.djangoproject.com/en/{{ docs_version }}/howto/deployment/wsgi/
 """
 
-import os, sys
+import os
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "{{ project_name }}.settings")
-sys.path.append('{{ project_directory }}')
 
 from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()

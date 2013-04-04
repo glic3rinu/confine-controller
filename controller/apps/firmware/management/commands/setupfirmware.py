8f76f7ba5873fffe18796414b608584f9d623369
Marc Aymerich <marcay@pangea.org>
2013-04-04 16:59:07 +0400

    Removed old options in setupfirmware command

--- controller/apps/firmware/management/commands/setupfirmware.py
+++ controller/apps/firmware/management/commands/setupfirmware.py
@@ -12,10 +12,6 @@ class Command(BaseCommand):
         self.option_list = BaseCommand.option_list + (
             make_option('--username', dest='username', default=get_default_celeryd_username(),
                 help='Specifies the system user that would generate the firmwares.'),
-            make_option('--base_image_path', dest='base_image_path', default=False,
-                help='Specifies the path where the base images are stored.'),
-            make_option('--build_path', dest='base_path', default=False,
-                help='Specifies the path where the build images are stored.'),
             make_option('--noinput', action='store_false', dest='interactive', default=True,
                 help='Tells Django to NOT prompt the user for input of any kind. '
                      'You must use --username with --noinput, and must contain the '
@@ -36,12 +32,3 @@ class Command(BaseCommand):
             run('addgroup fuse')
         if run('groups %s|grep fuse' % username, err_codes=[0,1]).return_code == 1: 
             run('adduser %s fuse' % username)
-        
-        # TODO remove
-        base_image_path = options.get('base_image_path')
-        if base_image_path:
-            update_settings(FIRMWARE_BASE_IMAGE_PATH=base_image_path)
-        
-        build_path = options.get('build_path')
-        if build_path:
-            update_settings(FIRMWARE_BUILD_PATH=build_path)

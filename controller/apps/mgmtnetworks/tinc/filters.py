f10e33adca045393835d210bc3150091da3d74c4
Marc Aymerich <marcay@pangea.org>
2013-04-04 17:03:20 +0400

    Use MyListFilter in MyHostsListFilter

--- controller/apps/mgmtnetworks/tinc/filters.py
+++ controller/apps/mgmtnetworks/tinc/filters.py
@@ -1,8 +1,7 @@
-from django.contrib.admin import SimpleListFilter
-from django.utils.encoding import force_text
+from controller.admin.filters import MySimpleListFilter
 
 
-class MyHostsListFilter(SimpleListFilter):
+class MyHostsListFilter(MySimpleListFilter):
     """ Filter hosts by admin according to request.user """
     title = 'Hosts'
     parameter_name = 'my_hosts'
@@ -16,16 +15,3 @@ class MyHostsListFilter(SimpleListFilter):
     def queryset(self, request, queryset):
         if self.value() == 'True':
             return queryset.filter(owner=request.user)
-    
-    def choices(self, cl):
-        # TODO make this reusable in a basefilterclass
-        """ Enable default selection different than All """
-        for lookup, title in self.lookup_choices:
-            yield {
-                'selected': self.value() == force_text(lookup),
-                'query_string': cl.get_query_string({
-                    self.parameter_name: lookup,
-                }, []),
-                'display': title,
-            }
-

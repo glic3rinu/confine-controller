5345824bea4236e1699f315a1c615df83088098a
Marc Aymerich <marcay@pangea.org>
2013-04-10 23:42:38 +0400

    Improved refresh action on state app

--- controller/apps/state/actions.py
+++ controller/apps/state/actions.py
@@ -1,4 +1,5 @@
 from django.db import transaction
+from django.db.models import get_model
 #from django_transaction_signals import defer
 
 from .tasks import get_state
@@ -6,9 +7,23 @@ from .tasks import get_state
 
 @transaction.commit_on_success
 def refresh(modeladmin, request, queryset):
+    """ get_state from NodeState/SliverState queryset synchronously """
     opts = queryset.model._meta
-    model_label = '%s.%s' % (opts.app_label, opts.object_name)
+    state_module = '%s.%s' % (opts.app_label, opts.object_name)
     field_name = queryset.model.get_related_field_name()
     ids = queryset.values_list('%s__id' % field_name , flat=True)
-    # make requests synchronously because we want inmediate feedback
-    get_state(model_label, ids=ids)
+    get_state(state_module, ids=ids)
+    related_model_name = queryset.model.get_related_model()._meta.object_name
+    msg = 'The state of %d %ss has been updated' % (queryset.count(), related_model_name)
+    modeladmin.message_user(request, msg)
+
+
+def refresh_state(modeladmin, request, queryset):
+    """ gate_state from Node/Sliver queryset synchronously """
+    opts = queryset.model._meta
+    state_module = 'state.%sState' % opts.object_name
+    state_model = get_model(*state_module.split('.'))
+    ids = queryset.values_list('state__id', flat=True)
+    get_state(state_module, ids=ids)
+    msg = 'The state of %d %ss has been updated' % (queryset.count(), opts.object_name)
+    modeladmin.message_user(request, msg)

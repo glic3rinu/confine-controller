531545b003bba5f44e8b2c579aad19ac055a5d8f
Marc Aymerich <marcay@pangea.org>
2013-04-13 16:38:20 +0400

    Reuse celery pool for executing synchronous get_state

--- controller/__init__.py
+++ controller/__init__.py
@@ -1,4 +1,4 @@
-VERSION = (0, 8, 37, 'alpha', 9)
+VERSION = (0, 8, 37, 'alpha', 10)
 
 
 def get_version():
--- controller/apps/state/actions.py
+++ controller/apps/state/actions.py
@@ -1,5 +1,3 @@
-from multiprocessing import Process
-
 from django.db import transaction
 from django.db.models import get_model
 from django.shortcuts import redirect
@@ -16,10 +14,9 @@ def refresh(modeladmin, request, queryset):
     state_module = '%s.%s' % (opts.app_label, opts.object_name)
     field_name = queryset.model.get_related_field_name()
     ids = queryset.values_list('%s__id' % field_name , flat=True)
-    # Execute get_state isolated on a process to avoid gevent wsgi pollution
-    process = Process(target=get_state, args=(state_module,), kwargs={'ids':ids})
-    process.start()
-    process.join()
+    # Execute get_state isolated on a process to avoid gevent polluting the stack
+    result = get_state.delay(state_module, ids=ids)
+    result.get()
     related_model_name = queryset.model.get_related_model()._meta.object_name
     msg = 'The state of %d %ss has been updated' % (queryset.count(), related_model_name)
     modeladmin.message_user(request, msg)
@@ -32,10 +29,9 @@ def refresh_state(modeladmin, request, queryset):
     state_module = 'state.%sState' % opts.object_name
     state_model = get_model(*state_module.split('.'))
     ids = queryset.values_list('state__id', flat=True)
-    # Execute get_state isolated on a process to avoid gevent wsgi pollution
-    process = Process(target=get_state, args=(state_module,), kwargs={'ids':ids})
-    process.start()
-    process.join()
+    # Execute get_state isolated on a process to avoid gevent polluting the stack
+    result = get_state.delay(state_module, ids=ids)
+    result.get()
     msg = 'The state of %d %ss has been updated' % (queryset.count(), opts.object_name)
     modeladmin.message_user(request, msg)
 

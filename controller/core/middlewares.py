b3eb61bae3e5d18d4228a6a48e038ebf97840569
Marc Aymerich <marcay@pangea.org>
2013-04-18 18:57:26 +0400

    Fixed api/server/ reverse admin url

--- controller/core/middlewares.py
+++ controller/core/middlewares.py
@@ -3,10 +3,11 @@ from controller.settings import DISABLE_LOGIN_CSRF_FROM
 
 class DisableLoginCSRF(object):
     """ 
-    Hack to disable csrf on the login page in order to allow login 
+    Hack to disable csrf on the login page in order to allow login
     integration with other sites
     """
     def process_view(self, request, view_func, view_args, view_kwargs):
-        if view_func.func_name == 'index' and view_func.__module__ == 'django.contrib.admin.sites':
-            if request.META.get('HTTP_ORIGIN', '') in DISABLE_LOGIN_CSRF_FROM:
+        if (view_func.func_name == 'index' and
+            view_func.__module__ == 'django.contrib.admin.sites' and
+            request.META.get('HTTP_ORIGIN', '') in DISABLE_LOGIN_CSRF_FROM):
                 setattr(request, '_dont_enforce_csrf_checks', True)

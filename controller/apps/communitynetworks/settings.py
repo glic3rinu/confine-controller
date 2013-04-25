6249b160905503bedfe0c1634ef14c3ad38e1caf
santiago <santiago@pangea.org>
2013-04-25 13:25:02 +0200

    Handled TODO staff.

--- controller/apps/communitynetworks/settings.py
+++ controller/apps/communitynetworks/settings.py
@@ -2,10 +2,12 @@ import os
 
 from django.conf import settings
 
+# another approach to get app path
+# http://blog.elsdoerfer.name/2008/06/06/django-finding-the-current-projects-path/
+module_dir = os.path.dirname(__file__) 
 
 # Certificate Authority bundle for validating the CNDB connection certificate
 # ca_bundle='/etc/ssl/certs/cacert.org.pem'
-module_dir = os.path.dirname(__file__) 
 ca_bundle = os.path.join(module_dir, 'certs/cacert.org.pem')
 COMMUNITYNETWORKS_CNDB_CA_BUNDLE = getattr(settings, 'COMMUNITYNETWORKS_CNDB_CA_BUNDLE', ca_bundle)
 

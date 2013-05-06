9e50c14992f99f660dc8c418a472f006a81e7885
santiago <santiago@pangea.org>
2013-05-06 14:09:49 +0200

    Include a message when rejecting a ticket.

--- controller/apps/issues/actions.py
+++ controller/apps/issues/actions.py
@@ -2,7 +2,9 @@ from django.contrib.sites.models import RequestSite
 from django.core.exceptions import PermissionDenied
 from django.db import transaction
 
-def is_operator_deco(func):
+from issues.models import Message
+
+def user_has_perm(func):
     """ 
     Check if the user has the required permission to execute an action    
     Inspired in user_passes_test from django.contrib.auth.decorators
@@ -16,7 +18,7 @@ def is_operator_deco(func):
     has_perm.__name__ = func.__name__
     return has_perm
 
-@is_operator_deco
+@user_has_perm
 @transaction.commit_on_success
 def resolve_tickets(modeladmin, request, queryset):
     site = RequestSite(request)
@@ -28,18 +30,54 @@ def resolve_tickets(modeladmin, request, queryset):
 resolve_tickets.url_name = 'resolve'
 
 
-@is_operator_deco
+@user_has_perm
 @transaction.commit_on_success
 def reject_tickets(modeladmin, request, queryset):
-    site = RequestSite(request)
-    queryset.reject(site)
-    for obj in queryset:
-        modeladmin.log_change(request, obj, "Marked as Rejected")
-    msg = "%s selected tickets are now rejected" % queryset.count()
-    modeladmin.message_user(request, msg)
+    from django.contrib.admin import helpers
+    from django.template.response import TemplateResponse
+    opts = modeladmin.model._meta
+    app_label = opts.app_label
+    print "APP_LABEL: %s" % (app_label)
+
+    if queryset.count() != 1:
+        messages.warning(request, "Please, one ticket at a time.")
+        return
+    
+
+    # The user has alreday confirmed the reject
+    if request.POST.get('post'):
+        #create new message with reject reason (message)
+        reason = request.POST.get('reason')
+        if reason:
+            ticket = queryset.get()
+            reason = "REJECT REASON: %s" % reason
+            message = Message(ticket=ticket, author=request.user, content=reason)
+            message.save()
+        site = RequestSite(request)
+        queryset.reject(site) 
+        for obj in queryset:
+            modeladmin.log_change(request, obj, "Marked as Rejected")
+        msg = "%s selected tickets are now rejected" % queryset.count()
+        modeladmin.message_user(request, msg)
+        return
+
+    context = { 
+        "title": "Are you sure?",
+        "content_message": "Are you sure you want to reject the selected ticket?",
+        "action_name": 'Reject',
+        "action_value": 'reject_tickets',
+        "deletable_objects": queryset,
+        'queryset': queryset,
+        "opts": opts,
+        "app_label": app_label,
+        'action_checkbox_name': helpers.ACTION_CHECKBOX_NAME,
+    }   
+
+    return TemplateResponse(request, 'admin/issues/reject_confirmation.html',
+        context, current_app=modeladmin.admin_site.name)
 reject_tickets.url_name = 'reject'
 
-@is_operator_deco
+@user_has_perm
 def open_tickets(modeladmin, request, queryset):
     site = RequestSite(request)
     queryset.open(site)
@@ -49,7 +87,7 @@ def open_tickets(modeladmin, request, queryset):
     modeladmin.message_user(request, msg)
 open_tickets.url_name = 'open'
 
-@is_operator_deco
+@user_has_perm
 @transaction.commit_on_success
 def take_tickets(modeladmin, request, queryset):
     queryset.take(owner=request.user)

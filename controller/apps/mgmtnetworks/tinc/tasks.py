aebca8fe13dd3283c9b4dbe915d0f847e8294636
Marc Aymerich <marcay@pangea.org>
2013-05-16 20:11:37 +0400

    Removed error codes when removing /etc/tinc/hosts*

--- controller/apps/mgmtnetworks/tinc/tasks.py
+++ controller/apps/mgmtnetworks/tinc/tasks.py
@@ -39,7 +39,7 @@ def update_tincd():
             scripts.append(script)
         
         # delete all tinc hosts
-        run('rm -f -- %s{host_,node_}*' % hosts_path, err_codes=[0,1])
+        run('rm -f -- %s{host_,node_}*' % hosts_path)
         
         # create all tinc hosts
         for script in scripts:

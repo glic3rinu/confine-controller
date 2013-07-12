9b7d2d2ad68a105f882b75a8d5d3845b8cd7e729
Marc Aymerich <marcay@pangea.org>
2013-07-12 13:07:29 +0400

    use absolute imports on users.registration.__init__.py

--- controller/apps/users/registration/__init__.py
+++ controller/apps/users/registration/__init__.py
@@ -1,3 +1,5 @@
+from __future__ import absolute_import
+
 from django.conf import settings
 from django.contrib.sites.models import RequestSite
 from django.core.exceptions import ImproperlyConfigured

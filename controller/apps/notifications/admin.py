abb2662f0cf3bec4e12419751e94b9f7a3ec5e0c
glic3rinu <vct@vct.(none)>
2013-07-24 15:35:30 +0000

    Added sync notifications link on notification change-list

--- controller/apps/notifications/admin.py
+++ controller/apps/notifications/admin.py
@@ -1,9 +1,14 @@
 from django import forms
+from django.conf.urls import patterns, url
 from django.contrib import admin
+from django.shortcuts import redirect
 
-from .actions import (enable_selected, disable_selected, run_notifications,
+from controller.admin.utils import action_to_view, wrap_admin_view
+from controller.utils.plugins import sync_plugins_action
+
+from notifications.actions import (enable_selected, disable_selected, run_notifications,
     upgrade_notifications)
-from .models import Notification, Delivered
+from notifications.models import Notification, Delivered
 
 
 class DeliveredInline(admin.TabularInline):
@@ -23,14 +28,26 @@ class NotificationAdmin(admin.ModelAdmin):
     inlines = [DeliveredInline]
     actions = [enable_selected, disable_selected, run_notifications, upgrade_notifications]
     
-    def has_add_permission(self, *args, **kwargs):
-        return False
+    def get_urls(self):
+        urls = super(NotificationAdmin, self).get_urls()
+        admin_site = self.admin_site
+        opts = self.model._meta
+        extra_urls = patterns("",
+            url("^/sync-plugins$",
+                wrap_admin_view(self, NotificationAdmin(Notification, admin_site).sync_plugins_view),
+                name='notifications_notification_sync_plugins'),
+        )
+        return extra_urls + urls
     
     def formfield_for_dbfield(self, db_field, **kwargs):
         """ Make some char input widgets larger """
         if db_field.name == 'subject':
             kwargs['widget'] = forms.TextInput(attrs={'size':'118'})
         return super(NotificationAdmin, self).formfield_for_dbfield(db_field, **kwargs)
+    
+    def sync_plugins_view(self, request):
+        sync_plugins_action('notifications')(self, request, None)
+        return redirect('admin:notifications_notification_changelist')
 
 
 admin.site.register(Notification, NotificationAdmin)

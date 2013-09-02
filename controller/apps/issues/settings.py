8f075e25b2dc4fffff3462c993a19b7d01aa1b1b
VCT user <vct@vct.(none)>
2013-09-02 10:44:19 +0000

    Fixed syntax error on issues.settings

--- controller/apps/issues/settings.py
+++ controller/apps/issues/settings.py
@@ -4,4 +4,4 @@ from django.conf import settings
 ISSUES_SUPPORT_EMAILS = getattr(settings, 'ISSUES_SUPPORT_EMAILS', [])
 
 
-ISSUES_NOTIFY_SUPERUSERS = getattr(settings 'ISSUES_NOTIFY_SUPERUSERS', True)
+ISSUES_NOTIFY_SUPERUSERS = getattr(settings, 'ISSUES_NOTIFY_SUPERUSERS', True)

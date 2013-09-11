c87a4c41f021782f1ed49eb937c7d826dc5a9584
santiago <santiago@pangea.org>
2013-09-11 11:24:55 +0200

    Created controller_version django-admin command

---
+++ controller/management/commands/controller_version.py
@@ -0,0 +1,9 @@
+from django.core.management.base import NoArgsCommand
+
+from controller import get_version
+
+class Command(NoArgsCommand):
+    help = 'Shows confine-controller version'
+
+    def handle_noargs(self, **options):
+        self.stdout.write(get_version())

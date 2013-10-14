bf5bf5e606f36e3cc387311310c02d0b10df2639
VCT user <vct@vct.(none)>
2013-10-14 12:36:15 +0000

    Added raw, from and to filter capabilities on monitor view

--- controller/apps/monitor/admin.py
+++ controller/apps/monitor/admin.py
@@ -1,11 +1,12 @@
 import json
 import time
-from datetime import timedelta
+from datetime import timedelta, datetime
 
 from django.conf.urls import patterns, url
 from django.contrib import admin
 from django.core.management import call_command
 from django.http import HttpResponse, Http404
+from django.utils import timezone
 from django.template.response import TemplateResponse
 
 from controller.admin.utils import get_modeladmin, wrap_admin_view
@@ -24,24 +25,32 @@ def monitor_view(request):
         'monitors': monitors,
         'opts': Server._meta,
         'base_url': '/admin/nodes/server/monitor',
+        'querystring': '?' + '&'.join(['%s=%s' % (k,v) for k,v in request.GET.items()])
     }
     return TemplateResponse(request, 'admin/monitor/monitoring.html', context,
             current_app='monitor')
 
 
-def factor(value, divisor):
-    return int(value)/divisor if value else value
-
-def TimeSerie_view(request, name):
+def time_serie_view(request, name):
+    raw = request.GET.get('raw', False)
+    date_from = request.GET.get('from', False)
+    date_to = request.GET.get('to', False)
     data = []
     try:
         monitor = TimeSerie.get_monitor(name)
     except KeyError:
         raise Http404
     series = monitor.series.order_by('date')
-#    series = series.extra(select={'date': "EXTRACT(EPOCH FROM date)"}).values('date', 'value')
+    if date_from and date_to:
+        date_from = datetime.fromtimestamp(float(date_from), timezone.utc)
+        date_to = datetime.fromtimestamp(float(date_to), timezone.utc)
+        series = series.filter(date__gte=date_from, date__lte=date_to)
+    if raw.capitalize() == 'True':
+        series = [ [False, [serie]] for serie in series ]
+    else:
+        series = group_by_interval(series, timedelta(minutes=5))
     previous = None
-    for __,series in group_by_interval(series, timedelta(minutes=5)):
+    for __, series in series:
         serie = monitor.aggregate(series)
         serie.apply_relativity(previous)
         previous = serie
@@ -56,7 +65,7 @@ def get_urls():
     urls = old_server_get_urls()
     extra_urls = patterns("",
         url("^monitor/(?P<name>\w+)",
-            wrap_admin_view(ServerAdmin, TimeSerie_view),
+            wrap_admin_view(ServerAdmin, time_serie_view),
             name='nodes_server_monitor_netio'),
         url("^monitor",
             wrap_admin_view(ServerAdmin, monitor_view),

62881a9b212f981d2a9a74f5f2e2295283e0770c
VCT user <vct@vct.(none)>
2013-10-15 21:04:54 +0000

    Upgrade to restframework 2.3.8

--- controller/bin/controller-admin.sh
+++ controller/bin/controller-admin.sh
@@ -142,7 +142,7 @@ function install_requirements () {
                      markdown \
                      django-filter \
                      django-admin-tools \
-                     djangorestframework==2.3.7 \
+                     djangorestframework==2.3.8 \
                      https://github.com/admin947/django-orphaned/archive/master.zip"
     LOCAL_PIP="paramiko \
                pygments \

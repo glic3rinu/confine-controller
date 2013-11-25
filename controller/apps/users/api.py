5d3ea34c8caefc9ff8087ce205a48ec053aa37db
santiago <santiago@pangea.org>
2013-11-25 11:58:00 +0100

    User API: implemented change-password

--- controller/apps/users/api.py
+++ controller/apps/users/api.py
@@ -1,10 +1,41 @@
 from __future__ import absolute_import
 
+from django.shortcuts import get_object_or_404
+from rest_framework import status, exceptions
+from rest_framework.response import Response
+from rest_framework.views import APIView
+
 from api import api, generics
 from permissions.api import ApiPermissionsMixin
 
 from .models import User, Group
 from .serializers import UserSerializer, GroupSerializer
+
+
+class ChangePassword(APIView):
+    """
+    **Relation type:** [`http://confine-project.eu/rel/controller/do-change-password`](
+        http://confine-project.eu/rel/controller/do-change-password)
+    
+    Endpoint containing the function URI used to reboot this node.
+    
+    POST data: `New user password`
+    """
+    url_name = 'change-password'
+    rel = 'http://confine-project.eu/rel/controller/do-change-password'
+
+    def post(self, request, *args, **kwargs):
+        if request.DATA is not None:
+            password = request.DATA
+            if not password: # check if the password is empty
+                raise exceptions.ParseError(detail='You must provide new password value')
+            user = get_object_or_404(User, pk=kwargs.get('pk'))
+            self.check_object_permissions(self.request, user)
+            user.set_password(password)
+            user.save()
+            response_data = {'detail': 'User password changed successfully'}
+            return Response(response_data, status=status.HTTP_202_ACCEPTED)
+        raise exceptions.ParseError(detail='You must provide new password value')
 
 
 class UserList(ApiPermissionsMixin, generics.URIListCreateAPIView):
@@ -30,6 +61,7 @@ class UserDetail(generics.RetrieveUpdateDestroyAPIView):
     """
     model = User
     serializer_class = UserSerializer
+    ctl = [ChangePassword]
 
 
 class GroupList(ApiPermissionsMixin, generics.URIListCreateAPIView):

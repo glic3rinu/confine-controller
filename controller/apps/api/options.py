f3a40e6b9f04aa8ace638bb7fa4678d76dc16e5d
santiago <santiago@pangea.org>
2014-05-26 13:52:20 +0200

    [api] Warn user on not implemented usage.

--- controller/apps/api/options.py
+++ controller/apps/api/options.py
@@ -104,6 +104,10 @@ class RestApi(object):
             # hook new attribute to each serializer
             for serializer in model_serializers:
                 serializer.base_fields.update({name: field(**kwargs)})
+        else:
+            import warnings
+            warnings.warn("Model %s not registered, so api.aggregate do nothing." % model)
+
 
 
 # singleton

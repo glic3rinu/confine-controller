60e678051d790ad7174bf6bd52269c76a0e95661
santiago <santiago@pangea.org>
2014-06-27 12:04:55 +0200

    Add test for Island changeview (regression rev 1974935d)

--- controller/apps/nodes/tests.py
+++ controller/apps/nodes/tests.py
@@ -5,12 +5,23 @@ when you run "manage.py test".
 Replace this with more appropriate tests for your application.
 """
 
+from django.core.urlresolvers import reverse
 from django.test import TestCase
 
+from users.models import User
+from .models import Island
 
-class SimpleTest(TestCase):
-    def test_basic_addition(self):
-        """
-        Tests that 1 + 1 always equals 2.
-        """
-        self.assertEqual(1 + 1, 2)
+
+class IslandTest(TestCase):
+    def setUp(self):
+        self.password = 'vct'
+        self.user = User.objects.create_superuser(username='vct', name='vct',
+            email='vct@localhost', password=self.password)
+        self.client.login(username=self.user.username, password=self.password)
+    
+    def test_changeview(self):
+        # Test island changeview (regression on #157 rev 1974935d)
+        island = Island.objects.create(name='Island', description='Island to test changeview.')
+        island_url = reverse('admin:nodes_island_change', args=(island.pk,))
+        resp = self.client.get(island_url)
+        self.assertTrue(200, resp.status_code)

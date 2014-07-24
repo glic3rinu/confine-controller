glic3rinu/confine-controller

commit 53187caec5a289e688697a70c22a845053adabb3

Fix registration profile's admin search (#518)

controller/apps/users/admin.py
@@ -18,6 +18,7 @@ from .filters import MyGroupsListFilter
 from .forms import (UserCreationForm, UserChangeForm, GroupRolesFormSet,
     UserRolesForm, JoinRequestForm, GroupAdminForm, GroupRolesInlineForm)
 from .models import User, AuthToken, Roles, Group, JoinRequest, ResourceRequest
+from .registration import admin as reg_admin # overrides registration.admin
 
 
 class AuthTokenInline(PermissionTabularInline):

controller/apps/users/registration/admin.py
@@ -1,0 +1,14 @@
+from __future__ import absolute_import
+
+from django.contrib import admin
+from registration.admin import RegistrationAdmin
+from registration.models import RegistrationProfile
+
+
+class CustomRegistrationAdmin(RegistrationAdmin):
+    # Override registration admin that fails with custom user model (#518)
+    search_fields = ('user__username', 'user__name')
+
+
+admin.site.unregister(RegistrationProfile)
+admin.site.register(RegistrationProfile, CustomRegistrationAdmin)

commit 882d7a965807b2191baae0c41f95d2bb4ae13395

Fix regression introduced by 53187cae (#520)
Decouple users app from users.registration subapp, used to customize
django-registration. If registration is not installed,
users.registration shouldn't do nothing.

controller/apps/users/admin.py
@@ -11,6 +11,7 @@ from django.utils.safestring import mark_safe
 
 from controller.admin import ChangeViewActions, SortableTabularInline
 from controller.admin.utils import get_admin_link
+from controller.utils.apps import is_installed
 from permissions.admin import PermissionModelAdmin, PermissionTabularInline
 
 from .actions import join_request, enable_account, send_email
@@ -18,7 +19,9 @@ from .filters import MyGroupsListFilter
 from .forms import (UserCreationForm, UserChangeForm, GroupRolesFormSet,
     UserRolesForm, JoinRequestForm, GroupAdminForm, GroupRolesInlineForm)
 from .models import User, AuthToken, Roles, Group, JoinRequest, ResourceRequest
-from .registration import admin as reg_admin # overrides registration.admin
+
+if is_installed('registration'):
+    from .registration import admin as reg_admin # overrides registration.admin
 
 
 class AuthTokenInline(PermissionTabularInline):
be3bd864006afd34171733a75a02edd4a697702e
santiago <santiago@pangea.org>
2014-07-28 11:28:47 +0200

    RFC2616: header parameter must be enclosed in double quotes.

--- controller/apps/api/renderers.py
+++ controller/apps/api/renderers.py
@@ -5,11 +5,11 @@ class ProfileJSONRenderer(JSONRenderer):
     """
     Update media type to include extra paramater 'profile'
     including the confine schema. e.g. while rendering a user
-    "application/json; profile='http://confine-project.eu/schema/registry/v0/user'"
+    'application/json; profile="http://confine-project.eu/schema/registry/v0/user"'
     """
     profile = None # should be defined by subclasses
     
     def __init__(self, *args, **kwargs):
         if self.profile:
-            self.media_type = "application/json; profile='%s'" % self.profile
+            self.media_type = 'application/json; profile="%s"' % self.profile
         super(ProfileJSONRenderer, self).__init__(*args, **kwargs)

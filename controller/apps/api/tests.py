49911b34d511d5469d91d37df8432bd187a995f0
santiago <santiago@pangea.org>
2014-09-15 17:49:41 +0200

    Add Pagination unit tests.

--- controller/apps/api/tests.py
+++ controller/apps/api/tests.py
@@ -139,3 +139,60 @@ class FilteringTests(TestCase):
         url = reverse('node-list')
         resp = self.client.get(url + '?/name=foo')
         self.assertEqual(resp.status_code, 422)
+
+
+import urlparse, sys
+class PaginationTests(TestCase):
+    fixtures = ['groups.json', 'nodes.json', 'slices.json', 'slivers.json',
+        'templates.json']
+    
+    def get_page(self, url):
+        url = urlparse.urlparse(url)
+        query = dict(urlparse.parse_qsl(url.query))
+        return int(query['page'])
+        
+    
+    def check_pagination_links(self, header_link, current_page=1):
+        value, rel = zip(*[link.split('; ') for link in header_link.split(', ')])
+        value = [val.lstrip("<").rstrip(">") for val in value]
+        rel = [r.split('=')[1].strip('"') for r in rel]
+        
+        # headers should always include first, last links
+        self.assertIn('first', rel)
+        self.assertIn('last', rel)
+        
+        # headers may include next, prev links
+        first_page = self.get_page(value[rel.index('first')])
+        last_page = self.get_page(value[rel.index('last')])
+        if current_page > first_page:
+            self.assertIn('prev', rel)
+        if current_page < last_page:
+            self.assertIn('next', rel)
+    
+    def test_correct_default_pagination(self):
+        url = reverse('node-list')
+        resp = self.client.get(url)
+        self.assertEqual(resp.status_code, 200)
+        self.check_pagination_links(resp['Link'])
+
+    def test_correct_invalid_page(self):
+        url = reverse('node-list')
+        resp = self.client.get(url + '?page=-1')
+        self.assertEqual(resp.status_code, 200)
+        self.check_pagination_links(resp['Link'])
+        
+        # FIXME: replace sys.maxsize by last_page + 1
+        resp = self.client.get(url + '?page=%i' % sys.maxsize)
+        self.assertEqual(resp.status_code, 200)
+        self.check_pagination_links(resp['Link'])
+    
+    def test_correct_invalid_per_page(self):
+        url = reverse('node-list')
+        resp = self.client.get(url + '?per_page=0')
+        self.assertEqual(resp.status_code, 200)
+        self.check_pagination_links(resp['Link'])
+        
+        url = reverse('node-list')
+        resp = self.client.get(url + '?per_page=NaN')
+        self.assertEqual(resp.status_code, 200)
+        self.check_pagination_links(resp['Link'])

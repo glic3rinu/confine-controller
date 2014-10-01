523712942956df43b96ca5187e90c48735f7db0c
santiago <santiago@pangea.org>
2014-10-01 09:48:58 +0200

    Add test checking availability of firmware plugins (#226)

--- controller/apps/firmware/tests.py
+++ controller/apps/firmware/tests.py
@@ -6,15 +6,16 @@ Replace this with more appropriate tests for your application.
 """
 
 from django.core.exceptions import SuspiciousFileOperation
+from django.core.management import call_command
 from django.test import TestCase
 
 from nodes.models import Node
 from users.models import Group
 
-from .models import Build
+from .models import Build, ConfigPlugin
 from .settings import FIRMWARE_BUILD_IMAGE_STORAGE
 
-class BuildTest(TestCase):
+class BuildTests(TestCase):
     def test_handle_access_denied_image_file(self):
         group = Group.objects.create(name='group', allow_nodes=True)
         node = Node.objects.create(name='node', group=group)
@@ -25,3 +26,20 @@ class BuildTest(TestCase):
             build.state
         except SuspiciousFileOperation:
             self.failureException
+
+
+class PluginTests(TestCase):
+    def test_enabled_by_default(self):
+        plugins = ConfigPlugin.objects
+        # database is clean so there is no plugins yet
+        self.assertEqual(plugins.count(), 0)
+        
+        # synchronize firmware plugins
+        call_command('syncfirmwareplugins')
+        
+        # check that all plugins has been initialized
+        # and USBImage and AuthKeys are enabled
+        self.assertEqual(plugins.count(), 3)
+        enabled = plugins.filter(is_active=True).values_list('label', flat=True)
+        self.assertIn('USBImagePlugin', enabled)
+        self.assertIn('AuthKeysPlugin', enabled)

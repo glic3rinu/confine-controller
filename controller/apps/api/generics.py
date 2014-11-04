glic3rinu/confine-controller

commit 50a025ce3534920235d0679fd0e72c010645a093

Add support to disable API resources list pagination.

controller/apps/api/generics.py
@@ -87,6 +87,10 @@ class URIListCreateAPIView(ControllerBase, generics.ListCreateAPIView):
         else:
             # per_page cannot be less than 1
             per_page = max(1, per_page)
+        
+        # Is pagination disabled?
+        if per_page is None:
+            per_page = qs.count()
         paginator = Paginator(qs, per_page)
         
         # validate num_page

commit afbe142ae79a872b30b3d6478a8252f479585313

Fixed ZeroDivisionError with empty querysets.
Regression introduced by 50a025ce3534920235d0679fd0e72c010645a093

controller/apps/api/generics.py
@@ -90,7 +90,7 @@ class URIListCreateAPIView(ControllerBase, generics.ListCreateAPIView):
         
         # Is pagination disabled?
         if per_page is None:
-            per_page = qs.count()
+            per_page = max(1, qs.count())
         paginator = Paginator(qs, per_page)
         
         # validate num_page

controller/apps/api/tests.py
@@ -248,6 +248,13 @@ class PaginationTests(TestCase):
         nodes_db = set(Node.objects.values_list('id', flat=True))
         nodes_id = set([node['id'] for node in nodes])
         self.assertEquals(nodes_db, nodes_id)
+    
+    def test_paginate_empty_queryset(self):
+        # Regression test ZeroDivisionError
+        assert not Sliver.objects.exists(), "Slivers queryset is not empty."
+        url = reverse('sliver-list')
+        resp = self.client.get(url)
+        self.assertEqual(resp.status_code, 200)
 
 
 class IntegrationTests(TestCase):
316b4925fd981fd855b4a9708f23aedbf703ecf9
santiago <santiago@pangea.org>
2014-12-10 11:09:35 +0100

    Reorder postupgrade operations: take gw snapshot before drop it from database.

--- controller/management/commands/postupgradecontroller.py
+++ controller/management/commands/postupgradecontroller.py
@@ -124,13 +124,6 @@ class Command(BaseCommand):
                     ' Please read the monitor app doc (MONITOR_MONITORS setting)\n'
                     'AUTOUPDATE: %s' % autoupdate_status)
             if version <= 1102:
-                # Handle InconsistentMigrationHistory on tinc app
-                # * v0.11.2 tinc includes 0022, 0028..0030
-                # * v0.11.3 tinc adds 0023..0027
-                # We can force south to merge migrations because
-                # are implemented to be runned without dependencies
-                run('python manage.py migrate tinc 0030 --merge --noinput')
-                
                 # Take a snapshot current gateways on API format for backwards
                 # compatibility purposes before dropping it
                 context = {
@@ -139,6 +132,12 @@ class Command(BaseCommand):
                 }
                 run('wget -nv --header="Accept: application/json" '
                     '--no-check-certificate %(gw_url)s -O %(output)s' % context)
+                # Handle InconsistentMigrationHistory on tinc app
+                # * v0.11.2 tinc includes 0022, 0028..0030
+                # * v0.11.3 tinc adds 0023..0027
+                # We can force south to merge migrations because
+                # are implemented to be runned without dependencies
+                run('python manage.py migrate tinc 0030 --merge --noinput')
         
         if not options.get('specifics_only'):
             # Common stuff

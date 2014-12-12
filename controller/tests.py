9379e9c0444192535b1ec5dc77f0129fa97ff808
santiago <santiago@pangea.org>
2014-12-12 14:22:30 +0100

    Refactor: create common AuthenticatedTestCase.

--- controller/tests.py
+++ controller/tests.py
@@ -2,6 +2,49 @@ from django.test import TestCase
 
 from controller.utils import decode_version
 
+from users.models import User
+
+
+class AuthenticatedTestCase(TestCase):
+    """Test case with several user roles authentication."""
+    fixtures = ['groups.json', 'users.json', 'roles.json']
+
+    class Meta:
+        abstract = True
+
+    def setUp(self):
+        """
+        By default the tests are executed as unprivileged user
+        """
+        # Update the passwords to be usable during testing
+        # because fixture and test can have different PASSWORD_HASHERS
+        for user in User.objects.all():
+            user.set_password("%spass" % user.username)
+            user.save()
+
+        self._login()
+
+    def _login(self, superuser=False, admin=False, tech=False):
+        if superuser:
+            user = 'super'
+        elif admin:
+            user = 'admin'
+        elif tech:
+            user = 'tech'
+        else:
+            user = 'user'
+        return self.login(user)
+    
+    def login(self, user='user'):
+        """Login a user into the system."""
+        self.client.logout()
+        if user not in ['super', 'admin', 'tech', 'researcher', 'user']:
+            raise ValueError('Invalid value for user: "%s"' % user)
+        
+        pwd = "%spass" % user
+        res = self.client.login(username=user, password=pwd)
+        self.assertTrue(res, "Logging in user %s, pwd %s failed." % (user, pwd))
+
 
 class ControllerTests(TestCase):
     def test_decode_version(self):

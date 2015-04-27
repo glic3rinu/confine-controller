9ac4951f041cef526cc3129eefd19f6129d9e85d
santiago <santiago@pangea.org>
2015-04-27 18:23:48 +0200

    Don't check Origin header on CSRF login form. refs C-Lab ticket-41

--- controller/core/middlewares.py
+++ controller/core/middlewares.py
@@ -8,6 +8,9 @@ class DisableLoginCSRF(object):
     """
     def process_view(self, request, view_func, view_args, view_kwargs):
         if (view_func.func_name == 'index' and
-            view_func.__module__ == 'django.contrib.admin.sites' and
-            request.META.get('HTTP_ORIGIN', '') in DISABLE_LOGIN_CSRF_FROM):
+            view_func.__module__ == 'django.contrib.admin.sites'): # and
+            # Don't check Origin header until is supported by most of
+            # the browsers (in april, 2015 only Chromium)
+            # https://stackoverflow.com/a/8087233/1538221
+            #request.META.get('HTTP_ORIGIN', '') in DISABLE_LOGIN_CSRF_FROM):
                 setattr(request, '_dont_enforce_csrf_checks', True)

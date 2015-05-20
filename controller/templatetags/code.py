20c73fa3c6ba073b78a117e44ca25f26a26fbbd8
santiago <santiago@pangea.org>
2015-05-20 16:24:53 +0200

    code templatetag accepts optional argument linenos.
    See `linenos` at http://pygments.org/docs/formatters/#HtmlFormatter

--- controller/templatetags/code.py
+++ controller/templatetags/code.py
@@ -9,9 +9,10 @@ register = template.Library()
 
 
 class CodeNode(template.Node):
-    def __init__(self, lang, code):
+    def __init__(self, lang, code, linenos):
         self.lang = lang
         self.nodelist = code
+        self.linenos = linenos
     
     def render(self, context):
         try:
@@ -20,19 +21,31 @@ class CodeNode(template.Node):
             language = self.lang
         code = self.nodelist.render(context)
         try:
+            linenos = template.Variable(self.linenos).resolve(context)
+        except:
+            linenos = self.linenos
+        try:
             lexer = lexers.get_lexer_by_name(language)
         except:
             try:
                 lexer = lexers.guess_lexer(code)
             except:
                 lexer = lexers.PythonLexer()
-        return highlight(code, lexer, HtmlFormatter(linenos='table'))
+        return highlight(code, lexer, HtmlFormatter(linenos=linenos))
 
 
 @register.tag(name='code')
-def do_code(parser,token):
-    code = token.split_contents()[-1]
+def do_code(parser, token):
+    token_contents = token.split_contents()
+    if len(token_contents) == 2:
+        code = token_contents[1]
+        linenos = 'table'
+    elif len(token_contents) == 3:
+        _, code, linenos = token_contents
+    else:
+        raise template.TemplateSyntaxError(
+            "%r tag requires arguments" % token.contents.split()[0]
+        )
     nodelist = parser.parse(('endcode',))
     parser.delete_first_token()
-    return CodeNode(code,nodelist)
-
+    return CodeNode(code, nodelist, linenos)

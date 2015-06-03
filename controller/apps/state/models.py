0295409366de8bce4e4d84ef44efaefa0bdde8ea
santiago <santiago@pangea.org>
2015-06-03 16:22:16 +0200

    Refactor: encapsulate NodeSoftwareVersion data into properties.

--- controller/apps/state/models.py
+++ controller/apps/state/models.py
@@ -8,6 +8,7 @@ from django.contrib.contenttypes.models import ContentType
 from django.db import models
 from django.dispatch import Signal
 from django.utils import timezone
+from django.utils.functional import cached_property
 from django_transaction_signals import defer
 
 from controller.utils.functional import cached
@@ -259,10 +260,20 @@ class NodeSoftwareVersion(models.Model):
     def __unicode__(self):
         return self.value
     
-    @property
+    @cached_property
     def version(self):
         """Return structured version data."""
         return extract_node_software_version(self.value)
+    
+    @cached_property
+    def name(self):
+        name = settings.STATE_NODE_SOFT_VERSION_NAME(self.version)
+        return "%s (%s)" % (name, self.version['date'])
+    
+    @cached_property
+    def url(self):
+        return settings.STATE_NODE_SOFT_VERSION_URL(self.version)
+
 
 node_heartbeat = Signal(providing_args=["instance", "node"])
 
